97f2192c421352548e4e5afc095a0323f64525bc
Louis Beaumont <louis.beaumont@gmail.com>
2024-07-05 14:46:33 +0200

    chore: iterate on ci + add cuda feat

--- build.rs
+++ build.rs
@@ -1,19 +1,11 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     #[cfg(target_os = "macos")]
     {
-        println!("cargo:rustc-link-lib=c++");
-        println!("cargo:rustc-link-search=/usr/lib");
-        println!("cargo:rustc-link-search=/opt/homebrew/lib");
+        // --
     }
 
-    #[cfg(target_os = "linux")]
-    {
-        println!("cargo:rustc-link-lib=stdc++");
-        println!("cargo:rustc-link-search=/usr/lib");
-    }
-
-    #[cfg(target_os = "windows")]
-    {
-        // Windows-specific linking, if needed
-    }
+    // ... (rest of the file for other platforms)
 }
