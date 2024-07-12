a6feed32b9fb873e5640f09d9db5bdae34c09296
matthew-heartful <matthew.heartful@gmail.com>
2024-07-11 21:55:54 -0700

    reduced clutter in fron end example

--- examples/ts/vercel-ai-chatbot/components/chat-panel.tsx
+++ examples/ts/vercel-ai-chatbot/components/chat-panel.tsx
@@ -36,25 +36,15 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'What is',
+      subheading: 'Neuralink?',
+      message: `what is Neuralink?`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
+      heading: 'Did i see',
+      subheading: 'Claude Sonnet app on twitter?',
+      message: 'Did i see "Claude"?'
     },
-    {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
-    },
-    {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
-    }
   ]
 
   return (
