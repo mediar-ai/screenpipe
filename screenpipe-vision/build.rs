8b9b051df7a906d647dce2e49577bb9e3435cb0f
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-08 19:03:37 +0200

    fix: fucking lib

--- screenpipe-vision/build.rs
+++ screenpipe-vision/build.rs
@@ -1,19 +1,12 @@
-use std::env;
-
 #[cfg(target_os = "macos")]
 fn main() {
     println!("cargo:rustc-link-lib=dylib=screenpipe");
 
-    // let destination = env::var("DESTINATION").unwrap_or_default();
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
+    // Add the library search path
+    println!("cargo:rustc-link-search=native=screenpipe-vision/lib");
 
-    // if destination == "brew" {
-    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
-    // } else if destination == "tauri" {
-    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../Frameworks");
-    // } else {
-    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../../screenpipe-vision/lib");
-    // }
+    // Set the rpath
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
 }
 
 #[cfg(not(target_os = "macos"))]
