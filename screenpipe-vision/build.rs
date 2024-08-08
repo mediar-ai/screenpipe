fcac2d836df90c68028cdfff585e7f2b0857e62e
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-08 19:55:23 +0200

    fix brew

--- screenpipe-vision/build.rs
+++ screenpipe-vision/build.rs
@@ -7,6 +7,10 @@ fn main() {
 
     // Set the rpath
     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../lib");
+
+    // Ensure the linker uses the correct library name
+    println!("cargo:rustc-link-arg=-Wl,-install_name,@rpath/libscreenpipe.dylib");
 }
 
 #[cfg(not(target_os = "macos"))]
