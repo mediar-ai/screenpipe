ac5a15ae5145ec7fb2fc7376dc6f167de762a60f
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-08 18:54:33 +0200

    fix: fucking lib

--- screenpipe-vision/build.rs
+++ screenpipe-vision/build.rs
@@ -2,17 +2,18 @@ use std::env;
 
 #[cfg(target_os = "macos")]
 fn main() {
-    let destination = env::var("DESTINATION").unwrap_or_default();
-
-    if destination == "brew" {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
-    } else if destination == "tauri" {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../Frameworks");
-    } else {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../../screenpipe-vision/lib");
-    }
-
     println!("cargo:rustc-link-lib=dylib=screenpipe");
+
+    // let destination = env::var("DESTINATION").unwrap_or_default();
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
+
+    // if destination == "brew" {
+    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../lib");
+    // } else if destination == "tauri" {
+    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../Frameworks");
+    // } else {
+    //     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../../screenpipe-vision/lib");
+    // }
 }
 
 #[cfg(not(target_os = "macos"))]
