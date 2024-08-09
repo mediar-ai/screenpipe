51ac130bc23b407b4ee627807db6cbc2597172f2
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-09 11:08:18 +0200

    fix: build issue

--- screenpipe-vision/build.rs
+++ screenpipe-vision/build.rs
@@ -1,9 +1,16 @@
+use std::env;
+use std::path::PathBuf;
+
 #[cfg(target_os = "macos")]
 fn main() {
     println!("cargo:rustc-link-lib=dylib=screenpipe");
 
+    // Get the package root directory
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let lib_path = PathBuf::from(manifest_dir).join("lib");
+
     // Add the library search path
-    println!("cargo:rustc-link-search=native=screenpipe-vision/lib");
+    println!("cargo:rustc-link-search=native={}", lib_path.display());
 }
 
 #[cfg(not(target_os = "macos"))]
