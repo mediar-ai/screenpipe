118e75c430bf83f9db2288a778ba856498770a99
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-13 09:46:09 +0200

    fix: updater permission

--- examples/apps/screenpipe-app-tauri/src-tauri/src/main.rs
+++ examples/apps/screenpipe-app-tauri/src-tauri/src/main.rs
@@ -59,6 +59,7 @@ async fn main() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             None,
