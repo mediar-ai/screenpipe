6686561b50f2d8a03d9dc1b4f4860177f5a552f4
Louis Beaumont <louis.beaumont@gmail.com>
2024-08-21 18:23:39 +0200

    fix: remove pipes from default features

--- screenpipe-core/src/lib.rs
+++ screenpipe-core/src/lib.rs
@@ -2,7 +2,9 @@ pub mod ffmpeg;
 pub use ffmpeg::find_ffmpeg_path;
 pub mod llm;
 pub use llm::*;
+#[cfg(feature = "pipes")]
 pub mod pipes;
+#[cfg(feature = "pipes")]
 pub use pipes::*;
 #[cfg(feature = "security")]
 mod pii_removal;
