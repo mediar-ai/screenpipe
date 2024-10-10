b9a6ab55fbc71fdde88ea3bbe16681c349f20241
Ezra Ellette <ezrasellette@gmail.com>
2024-10-10 01:44:34 -0500

    add spectral subtraction with constant noise

--- screenpipe-audio/src/core.rs
+++ screenpipe-audio/src/core.rs
@@ -16,6 +16,7 @@ pub enum AudioTranscriptionEngine {
     WhisperTiny,
     WhisperDistilLargeV3,
     WhisperLargeV3Turbo,
+    WhisperLargeV3,
 }
 
 impl fmt::Display for AudioTranscriptionEngine {
@@ -25,6 +26,7 @@ impl fmt::Display for AudioTranscriptionEngine {
             AudioTranscriptionEngine::WhisperTiny => write!(f, "WhisperTiny"),
             AudioTranscriptionEngine::WhisperDistilLargeV3 => write!(f, "WhisperLarge"),
             AudioTranscriptionEngine::WhisperLargeV3Turbo => write!(f, "WhisperLargeV3Turbo"),
+            AudioTranscriptionEngine::WhisperLargeV3 => write!(f, "WhisperLargeV3"),
         }
     }
 }
