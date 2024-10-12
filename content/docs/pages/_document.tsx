5740660398f034ae26711198dde827793020a925
matthew-heartful <matthew.heartful@gmail.com>
2024-10-11 19:45:44 -0700

    merging main into fork branch

--- content/docs/pages/_document.tsx
+++ content/docs/pages/_document.tsx
@@ -1,12 +1,33 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://d345f39z3arwqc.cloudfront.net/entelligence-chat.js"
+          strategy="beforeInteractive"
+          type="module"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Script id="entelligence-init" strategy="afterInteractive">
+          {`
+            if (window.EntelligenceChat) {
+              window.EntelligenceChat.init({
+                analyticsData: {
+                  repoName: "screenpipe",
+                  organization: "mediar-ai",
+                  apiKey: "fdANNfYgwriDY0iADKXzgrJdNIilNdYLvWlWtvMJCoQ",
+                  theme: "dark"
+                }
+              });
+            }
+          `}
+        </Script>
       </body>
     </Html>
   );
