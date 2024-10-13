55e3974b3972f89c33da2a50a909f2753682db9a
Louis Beaumont <louis.beaumont@gmail.com>
2024-10-13 15:55:52 -0700

    small sunday stuff

--- screenpipe-app-tauri/components/onboarding/navigation.tsx
+++ screenpipe-app-tauri/components/onboarding/navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
+import { ArrowLeft, ArrowRight } from 'lucide-react'; // Importing icons
 
 interface OnboardingNavigationProps {
   prevBtnText?: string,
@@ -19,24 +20,25 @@ const OnboardingNavigation: React.FC<OnboardingNavigationProps> = ({
   handleNextSlide,
 }) => {
   return (
-    <div className={` flex justify-between mx-8 ${className}`}>
+    <div className={`flex justify-between items-center mx-auto ${className} fixed bottom-0 left-20 right-20 p-4 bg-transparent max-w-screen-lg`}>
       <Button 
-        className="w-fit min-w-32 disabled:!cursor-not-allowed disabled:!pointer-events-auto"
+        className="flex items-center w-fit min-w-32 disabled:!cursor-not-allowed disabled:!pointer-events-auto"
         variant={"outline"}
         onClick={handlePrevSlide}
         disabled={isLoading}
       >
+        <ArrowLeft className="mr-2" /> {/* Icon with margin */}
         {prevBtnText}
       </Button>
       <Button 
-        className="w-fit min-w-32 disabled:!cursor-not-allowed disabled:!pointer-events-auto" 
+        className="flex items-center w-fit min-w-32 disabled:!cursor-not-allowed disabled:!pointer-events-auto" 
         onClick={handleNextSlide}
         disabled={isLoading}
       >
         {nextBtnText}
+        <ArrowRight className="ml-2" /> {/* Icon with margin */}
       </Button>
     </div>
   );
 } 
 export default OnboardingNavigation;
-
