e1c0567958b93dbc4eb12b216ed2fefe41661b79
matthew-heartful <matthew.heartful@gmail.com>
2024-10-13 22:15:51 -0700

    simplified

--- screenpipe-actions/src/type_and_animate.rs
+++ screenpipe-actions/src/type_and_animate.rs
@@ -3,15 +3,6 @@ use enigo::{Enigo, KeyboardControllable};
 use std::time::Duration;
 use tokio::time;
 
-async fn type_text(text: &str) -> Result<()> {
-    let mut enigo = Enigo::new();
-    for c in text.chars() {
-        enigo.key_sequence(&c.to_string());
-        // sleep(Duration::from_millis(1)).await;
-    }
-    Ok(())
-}
-
 pub async fn delete_characters(count: usize) -> Result<()> {
     let mut enigo = Enigo::new();
     for _ in 0..count {
@@ -24,7 +15,11 @@ pub async fn type_slowly(text: &str) -> Result<()> {
     let mut enigo = Enigo::new();
     for c in text.chars() {
         match c {
-            '\n' => enigo.key_click(enigo::Key::Return),
+            '\n' => {
+                enigo.key_down(enigo::Key::Shift);
+                enigo.key_click(enigo::Key::Return);
+                enigo.key_up(enigo::Key::Shift);
+            },
             '\t' => enigo.key_click(enigo::Key::Tab),
             _ => enigo.key_sequence(&c.to_string()),
         }
@@ -32,4 +27,3 @@ pub async fn type_slowly(text: &str) -> Result<()> {
     }
     Ok(())
 }
-
