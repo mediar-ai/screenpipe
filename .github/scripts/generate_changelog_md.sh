b77f1fbde8b8f4e6e3353ce0e47eee5a99046332
Wesley Matos <me@ologbonowiwi.tech>
2024-10-19 22:21:13 -0300

    feat: generate last version

--- .github/scripts/generate_changelog_md.sh
+++ .github/scripts/generate_changelog_md.sh
@@ -1,40 +1,10 @@
 TAGS=$(git tag --sort=-creatordate)
-CURRENT_RELEASE=$(echo "$TAGS" | sed -n '2p')
-LAST_RELEASE=$(echo "$TAGS" | sed -n '3p')
-
-# echo "Current release: $CURRENT_RELEASE"
-# echo "Last release: $LAST_RELEASE"
+CURRENT_RELEASE=$(echo "$TAGS" | sed -n '1p')
+LAST_RELEASE=$(echo "$TAGS" | sed -n '2p')
 
 COMMITS=$(git log --oneline $LAST_RELEASE..$CURRENT_RELEASE --oneline | tr '\n' ', ' | sed 's/"/\\"/g')
 
-# echo "Commits: $COMMITS"
-
-# LAST_CHANGELOG=$(sed ':a;N;$!ba;s/\n/\\n/g' content/changelogs/CHANGELOG.md)
-# echo $LAST_CHANGELOG
 LAST_CHANGELOG=$(awk '{printf "%s\\n", $0}' content/changelogs/CHANGELOG.md | sed 's/"/\\"/g')
-echo $LAST_CHANGELOG
-
-echo "{
-      \"model\": \"gpt-3.5-turbo\",
-      \"messages\": [
-        {
-          \"role\": \"system\",
-          \"content\": \"You are an assistant specialized in generating changelogs from commits. Categorize the changes into the following sections if they apply: New Features, Improvements, Fixes, and Others.\"
-        },
-        {
-          \"role\": \"system\",
-          \"content\": \"Be concise and focus on what brings customer value. Avoid copy-pasting the commits, and instead rephrase in a 'selling' way. Do not consider things like 'deploy', 'review/merge pull requests', and other day-to-day tasks that are sort of standard for software projects.\"
-        },
-        {
-          \"role\": \"system\",
-          \"content\": \"Here is an example of it, please write it based on this version: $LAST_CHANGELOG\"
-        },
-        {
-          \"role\": \"user\",
-          \"content\": \"Here are my commits: $COMMITS\"
-        }
-      ]
-    }"
 
 CONTENT=$(
   curl https://api.openai.com/v1/chat/completions \
