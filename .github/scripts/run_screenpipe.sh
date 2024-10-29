c13b77f50d31e9260a3868ad34e779f8c52d46d5
Neptune <neptune650@proton.me>
2024-10-29 14:47:31 -0500

    Add resource usage checks

--- .github/scripts/run_screenpipe.sh
+++ .github/scripts/run_screenpipe.sh
@@ -4,4 +4,9 @@ export PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native
 ./target/release/screenpipe --debug > screenpipe_output.log 2>&1 &
 SCREENPIPE_PID=$!
 echo $SCREENPIPE_PID > screenpipe.pid
-sleep 60
+# Check resource usage every 10 seconds, for 1 minute
+for i in {1..6}
+do
+   sleep 10
+   ps -p $SCREENPIPE_PID -o %cpu,%mem,cmd
+done
