71ed9c02947a6964ab7d6c3a8300a7b52c45bb25
Louis Beaumont <louis.beaumont@gmail.com>
2024-10-30 09:02:16 -0700

    fix css

--- content/docs/pages/_document.tsx
+++ content/docs/pages/_document.tsx
@@ -1,8 +1,9 @@
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
