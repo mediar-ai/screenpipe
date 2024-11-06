bac913872c1fc75b4ec3d308af5a70b2c5c68cc3
Wesley Matos <me@ologbonowiwi.tech>
2024-11-06 18:35:15 -0300

    fix: use crab nebula to get last version instead of git tags

--- .github/scripts/generate_changelog_md.sh
+++ .github/scripts/generate_changelog_md.sh
@@ -1,8 +1,23 @@
-TAGS=$(git tag --sort=-creatordate)
-CURRENT_RELEASE=$(echo "$TAGS" | sed -n '1p')
-LAST_RELEASE=$(echo "$TAGS" | sed -n '2p')
+CURRENT_RELEASE=$1
 
-COMMITS=$(git log --oneline $LAST_RELEASE..$CURRENT_RELEASE --oneline | tr '\n' ', ' | sed 's/"/\\"/g')
+LAST_RELEASE=$(cn release list screenpipe --api-key $CN_API_KEY --format json | jq '.[0]')
+LAST_COMMIT_DATE=$(echo $LAST_RELEASE | jq '.createdAt')
+LAST_RELEASE_VERSION=$(echo $LAST_RELEASE | jq '.version')
+COMMIT_LAST_RELEASE=$(git log -1 --until="$LAST_COMMIT_DATE" --format="%H")
+
+COMMIT_CURRENT_RELEASE=$(git log -1 --format="%H")
+COMMIT_CURRENT_RELEASE=${2:-$COMMIT_CURRENT_RELEASE}
+
+# If both are equal, then there's nothing to add to the changelog
+if [ "$COMMIT_LAST_RELEASE" == "$COMMIT_CURRENT_RELEASE" ]; then
+  echo "No new commits to add to the changelog"
+  echo "CHANGELOG_GENERATED=0" >> $GITHUB_ENV
+  exit 0
+fi
+
+COMMITS=$(git log --oneline $COMMIT_LAST_RELEASE..$COMMIT_CURRENT_RELEASE --oneline | tr '\n' ', ' | sed 's/"/\\"/g')
+
+echo $COMMITS
 
 LAST_CHANGELOG=$(awk '{printf "%s\\n", $0}' screenpipe-app-tauri/public/CHANGELOG.md | sed 's/"/\\"/g')
 
@@ -23,7 +38,7 @@ CONTENT=$(
         },
         {
           \"role\": \"system\",
-          \"content\": \"Here is an example of it, please write it based on this version: $LAST_CHANGELOG\"
+          \"content\": \"Here is an example of it, please write it based on this version: $LAST_CHANGELOG. Be sure to write in markdown language, and avoid adding the #### **Full Changelog:** section, as this will be added manually via scripts\"
         },
         {
           \"role\": \"user\",
@@ -35,6 +50,12 @@ CONTENT=$(
 
 CONTENT=$(jq '.choices[0].message.content' <<< $CONTENT)
 
+# exit if the content is null
+if [ "$CONTENT" == "null" ]; then
+  echo "Failed to generate changelog content. It may be an error with the OpenAI API key."
+  exit 1
+fi
+
 # Create directory content/changelogs if it doesn't exist
 mkdir -p content/changelogs
 
@@ -43,7 +64,7 @@ echo ${CONTENT//\"/} > content/changelogs/$CURRENT_RELEASE.md
 
 # Add the full changelog on the end of the file
 echo """
-#### **Full Changelog:** [$LAST_RELEASE...$CURRENT_RELEASE](https://github.com/mediar-ai/screenpipe/compare/$LAST_RELEASE...$CURRENT_RELEASE)
+#### **Full Changelog:** [$LAST_RELEASE_VERSION...$CURRENT_RELEASE](https://github.com/mediar-ai/screenpipe/compare/$COMMIT_LAST_RELEASE...$COMMIT_CURRENT_RELEASE)
 """ >> content/changelogs/$CURRENT_RELEASE.md
 
 # Copy the new changelog to the main changelog file
@@ -51,3 +72,6 @@ cp content/changelogs/$CURRENT_RELEASE.md screenpipe-app-tauri/public/CHANGELOG.
 
 # Output the current release version to be used in the workflow
 echo "CURRENT_RELEASE=$CURRENT_RELEASE" >> $GITHUB_ENV
+
+# Set the flag to indicate that the changelog was generated
+echo "CHANGELOG_GENERATED=1" >> $GITHUB_ENV
