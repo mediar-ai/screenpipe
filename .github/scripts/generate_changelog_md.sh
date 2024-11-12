c93cad55e00ea9d2602eca0a747bb6cc7ced58aa
Wesley Matos <me@ologbonowiwi.tech>
2024-11-12 11:29:38 -0300

    refactor: lock changelog generation using v0.1.98

--- .github/scripts/generate_changelog_md.sh
+++ .github/scripts/generate_changelog_md.sh
@@ -2,7 +2,7 @@ CURRENT_RELEASE=$1
 
 CHANGELOG_PUBLIC_PATH=screenpipe-app-tauri/public/CHANGELOG.md
 
-LAST_CHANGELOG=$(awk '/^#### \*\*Full Changelog:\*\*/{exit} {print}' $CHANGELOG_PUBLIC_PATH | awk '{printf "%s\\n", $0}' | sed 's/"/\\"/g')
+LAST_CHANGELOG=$(awk '{printf "%s\\n", $0}' contents/changelogs/v0.1.95.md | sed 's/"/\\"/g')
 
 # The if else is necessary to ensure it works both locally and on cloud
 
