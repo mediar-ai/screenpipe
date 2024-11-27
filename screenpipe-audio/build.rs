dafa74a06fb4deae498dbe796ce6e96b988c259f
Wesley Matos <me@ologbonowiwi.tech>
2024-11-26 21:14:43 -0300

    refactor: install bun on build.rs

--- screenpipe-audio/build.rs
+++ screenpipe-audio/build.rs
@@ -1,10 +1,67 @@
 #[cfg(target_os = "windows")]
 use std::{env, fs, process::Command};
+use std::{
+    io::Result,
+    process::{Command, Output},
+};
 
 fn main() {
     #[cfg(target_os = "windows")]
     {
         install_onnxruntime();
+    }
+
+    if !is_bun_installed() {
+        install_bun();
+    }
+}
+
+fn is_bun_installed() -> bool {
+    let output = Command::new("bun").arg("--version").output();
+
+    match output {
+        Err(_) => false,
+        Ok(output) => output.status.success(),
+    }
+}
+
+fn run_bun_install_command(command: Result<Output>) {
+    match command {
+        Err(error) => {
+            println!("failed to install bun: {}", error);
+            println!("please install bun manually.");
+        }
+        Ok(output) => {
+            if output.status.success() {
+                println!("bun installed successfully.");
+            } else {
+                println!(
+                    "failed to install bun: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
+                println!("please install bun manually.");
+            }
+        }
+    }
+}
+
+fn install_bun() {
+    println!("installing bun...");
+
+    #[cfg(target_os = "windows")]
+    {
+        println!("attempting to install bun using npm...");
+
+        run_bun_install_command(Command::new("npm").args(["install", "-g", "bun"]).output());
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    {
+        run_bun_install_command(
+            Command::new("sh")
+                .args(["-c", "curl -fsSL https://bun.sh/install | bash"])
+                .output(),
+        );
     }
 }
 
