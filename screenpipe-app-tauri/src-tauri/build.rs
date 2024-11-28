50aa3d46cba577b847dee3d4230d699b35755cba
Louis Beaumont <louis.beaumont@gmail.com>
2024-11-28 09:38:35 -0800

    fix: build.rs

--- screenpipe-app-tauri/src-tauri/build.rs
+++ screenpipe-app-tauri/src-tauri/build.rs
@@ -21,7 +21,18 @@ fn main() {
         }
         #[cfg(target_arch = "aarch64")]
         {
-            fs::copy("../../target/aarch64-apple-darwin/release/screenpipe", "screenpipe-aarch64-apple-darwin").unwrap();
+            let primary_path = "../../target/aarch64-apple-darwin/release/screenpipe";
+            let fallback_path = "../../target/release/screenpipe";
+            
+            let source_path = if fs::metadata(primary_path).is_ok() {
+                primary_path
+            } else {
+                fallback_path
+            };
+
+            fs::copy(source_path, "screenpipe-aarch64-apple-darwin")
+                .expect("failed to copy screenpipe binary");
+            
             Command::new("install_name_tool")
                 .args(["-change", "../../screenpipe-vision/lib/libscreenpipe_arm64.dylib",
                        "@executable_path/../Frameworks/libscreenpipe_arm64.dylib", "./screenpipe-aarch64-apple-darwin"])
