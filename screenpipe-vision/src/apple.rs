3f3b010a3de19df43cf96277ba3ce018e4316f84
Mahesh Vagicherla <maheshmahi19@outlook.com>
2024-12-03 02:52:39 +0530

    feat: use apple native ocr using rust

--- screenpipe-vision/src/apple.rs
+++ screenpipe-vision/src/apple.rs
@@ -1,64 +1,110 @@
 use image::DynamicImage;
 use log::error;
-use std::ffi::CStr;
-use std::os::raw::{c_char, c_uchar};
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use uuid::Uuid;
 
-use std::ops::Drop;
-
-struct OcrResultGuard(*mut c_char);
-
-impl Drop for OcrResultGuard {
-    fn drop(&mut self) {
-        unsafe {
-            if !self.0.is_null() {
-                free_string(self.0);
-            }
-        }
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct OcrResultBBox {
+    x: f64,
+    y: f64,
+    height: f64,
+    width: f64,
 }
 
-#[cfg(target_os = "macos")]
-#[link(name = "screenpipe")]
-extern "C" {
-    fn perform_ocr(
-        image_data: *const c_uchar,
-        length: usize,
-        width: i32,
-        height: i32,
-        languages: *const *const c_char,
-        languages_count: i32,
-    ) -> *mut c_char;
-    fn free_string(ptr: *mut c_char);
+#[derive(Serialize, Deserialize, Debug)]
+struct OcrTextElement {
+    bounding_box: Vec<OcrResultBBox>,
+    confidence: f32,
+    text: String,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct OcrResult {
+    ocr_result: String,
+    text_elements: Vec<OcrTextElement>,
+    overall_confidence: f32,
+}
+
 #[cfg(target_os = "macos")]
 pub fn perform_ocr_apple(
     image: &DynamicImage,
-    languages: Vec<screenpipe_core::Language>,
+    _languages: Vec<screenpipe_core::Language>,
 ) -> String {
-    let rgba = image.to_rgba8();
-    let (width, height) = rgba.dimensions();
-    let raw_data = rgba.as_raw();
+    use cidre::{
+        ns,
+        vn::{self, ImageRequestHandler, RecognizeTextRequest},
+    };
 
-    let languages = get_apple_languages(languages);
+    let default_ocr_result = serde_json::to_string(&OcrResult {
+        ocr_result: String::new(),
+        overall_confidence: 0.0,
+        text_elements: Vec::new(),
+    }).unwrap();
 
-    let c_languages: Vec<*const c_char> = languages
-        .iter()
-        .map(|s| s.as_ptr() as *const c_char)
-        .collect();
+    let file_name = Uuid::new_v4().as_urn().to_string();
+    let temp_path = PathBuf::from(std::env::temp_dir()).join(file_name + ".png");
+    let _ = image.save(&temp_path);
 
-    unsafe {
-        let result_ptr = perform_ocr(
-            raw_data.as_ptr(),
-            raw_data.len(),
-            width as i32,
-            height as i32,
-            c_languages.as_ptr(),
-            c_languages.len() as i32,
-        );
-        let _guard = OcrResultGuard(result_ptr);
-        let result = CStr::from_ptr(result_ptr).to_string_lossy().into_owned();
-        result
+    let file_uri = format!("file://{}", temp_path.to_str().unwrap());
+    let url = ns::Url::with_string(&ns::String::with_str(&file_uri));
+
+    let handler = ImageRequestHandler::with_url(&url.unwrap(), None);
+    let mut request = RecognizeTextRequest::new();
+    // Recognize all languages
+    request.set_revision(3);
+    let requests = ns::Array::<vn::Request>::from_slice(&[&request]);
+    let result = handler.perform(&requests);
+
+    if result.is_err() {
+        return default_ocr_result;
     }
+
+    // let results = request.results();
+
+    if let Some(results) = request.results() {
+        if !results.is_empty() {
+            let mut ocr_results_vec: Vec<OcrTextElement> = Vec::new();
+            let mut overall_confidence: f32 = 0.0;
+            let mut ocr_text: String = String::new();
+            results.iter().for_each(|result| {
+                let observation_result = result.top_candidates(1).get(0).unwrap();
+                let text = observation_result.string();
+                let confidence = observation_result.confidence();
+                let bbox = observation_result
+                    .bounding_box_for_range(ns::Range::new(0, text.len()))
+                    .unwrap()
+                    .bounding_box();
+                let x = bbox.origin.x;
+                let y = bbox.origin.y;
+                let height = bbox.size.height;
+                let width = bbox.size.width;
+
+                ocr_results_vec.push(OcrTextElement {
+                    text: text.to_string(),
+                    bounding_box: vec![OcrResultBBox {
+                        x,
+                        y,
+                        height,
+                        width,
+                    }],
+                    confidence,
+                });
+
+                overall_confidence += confidence;
+                ocr_text += &text.to_string();
+            });
+            let result_string = serde_json::to_string(&OcrResult {
+                overall_confidence,
+                ocr_result: ocr_text.to_string(),
+                text_elements: ocr_results_vec,
+            })
+            .unwrap();
+            return result_string;
+        }
+    }
+
+    return default_ocr_result;
 }
 
 #[cfg(target_os = "macos")]
@@ -66,21 +112,21 @@ pub fn parse_apple_ocr_result(json_result: &str) -> (String, String, Option<f64>
     let parsed_result: serde_json::Value = serde_json::from_str(json_result).unwrap_or_else(|e| {
         error!("Failed to parse JSON output: {}", e);
         serde_json::json!({
-            "ocrResult": "",
-            "textElements": [],
-            "overallConfidence": 0.0
+            "ocr_result": "",
+            "text_elements": [],
+            "overall_confidence": 0.0
         })
     });
 
-    let text = parsed_result["ocrResult"]
+    let text = parsed_result["ocr_result"]
         .as_str()
         .unwrap_or("")
         .to_string();
-    let text_elements = parsed_result["textElements"]
+    let text_elements = parsed_result["text_elements"]
         .as_array()
         .unwrap_or(&vec![])
         .clone();
-    let overall_confidence = parsed_result["overallConfidence"].as_f64();
+    let overall_confidence = parsed_result["overall_confidence"].as_f64();
 
     let json_output: Vec<serde_json::Value> = text_elements
         .iter()
@@ -92,10 +138,10 @@ pub fn parse_apple_ocr_result(json_result: &str) -> (String, String, Option<f64>
                 "par_num": "0",
                 "line_num": "0",
                 "word_num": "0",
-                "left": element["boundingBox"]["x"].as_f64().unwrap_or(0.0).to_string(),
-                "top": element["boundingBox"]["y"].as_f64().unwrap_or(0.0).to_string(),
-                "width": element["boundingBox"]["width"].as_f64().unwrap_or(0.0).to_string(),
-                "height": element["boundingBox"]["height"].as_f64().unwrap_or(0.0).to_string(),
+                "left": element["bounding_box"]["x"].as_f64().unwrap_or(0.0).to_string(),
+                "top": element["bounding_box"]["y"].as_f64().unwrap_or(0.0).to_string(),
+                "width": element["bounding_box"]["width"].as_f64().unwrap_or(0.0).to_string(),
+                "height": element["bounding_box"]["height"].as_f64().unwrap_or(0.0).to_string(),
                 "conf": element["confidence"].as_f64().unwrap_or(0.0).to_string(),
                 "text": element["text"].as_str().unwrap_or("").to_string()
             })
@@ -108,31 +154,4 @@ pub fn parse_apple_ocr_result(json_result: &str) -> (String, String, Option<f64>
     });
 
     (text, json_output_string, overall_confidence)
-}
-
-#[cfg(target_os = "macos")]
-fn get_apple_languages(languages: Vec<screenpipe_core::Language>) -> Vec<String> {
-    use screenpipe_core::Language;
-
-    let mut langs: Vec<String> = Vec::new();
-    for lang in languages {
-        let lang_str = match lang {
-            Language::English => "en-US",
-            Language::Spanish => "es-ES",
-            Language::French => "fr-FR",
-            Language::German => "de-DE",
-            Language::Italian => "it-IT",
-            Language::Portuguese => "pt-BR",
-            Language::Russian => "ru-RU",
-            Language::Chinese => "zh-Hans",
-            Language::Korean => "ko-KR",
-            Language::Japanese => "ja-JP",
-            Language::Ukrainian => "uk-UA",
-            Language::Thai => "th-TH",
-            Language::Arabic => "ar-SA",
-            _ => continue,
-        };
-        langs.push(lang_str.to_string());
-    }
-    langs
 }
