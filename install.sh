b1325b8dffa4da97b12bbaf6ff4fc79c556465fe
Louis Beaumont <louis.beaumont@gmail.com>
2024-12-11 19:58:25 -0800

    feat: add color to install.sh

--- install.sh
+++ install.sh
@@ -283,10 +283,6 @@ fi
 cd || exit 1
 rm -rf "$TMP_DIR"
 
-# Define colors
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
-
 echo "
 ███████╗ ██████╗██████╗ ███████╗███████╗███╗   ██╗██████╗ ██╗██████╗ ███████╗
 ██╔════╝██╔════╝██╔══██╗██╔════╝██╔════╝████╗  ██║██╔══██╗██║██╔══██╗██╔════╝
@@ -302,5 +298,5 @@ echo "1. restart your terminal or run: source $SHELL_CONFIG"
 echo "2. run: screenpipe"
 echo "3. allow permissions on macos (screen, mic) if needed"
 echo ""
-echo "join our discord: ${BLUE}https://discord.gg/dU9EBuw7Uq${NC}"
-echo "check the docs: ${BLUE}https://docs.screenpi.pe${NC}"
+echo "join our discord: \033[0;34mhttps://discord.gg/dU9EBuw7Uq\033[0m"
+echo "check the docs: \033[0;34mhttps://docs.screenpi.pe\033[0m"
