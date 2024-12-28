2d4620b5eb6b28533e7f0ff18a189a030ec82073
oliverqx <8559757+oliverqx@users.noreply.github.com>
2024-12-28 15:56:18 -0600

    feat(components): update animated borders

--- screenpipe-app-tauri/components/ui/animated-border-svg.tsx
+++ screenpipe-app-tauri/components/ui/animated-border-svg.tsx
@@ -1,67 +1,90 @@
 import { cn } from '@/lib/utils';
 import { motion } from 'framer-motion';
-const draw = {
-    hidden: { 
-      strokeWidth: '10px',
-      pathLength: 0, 
-      transition: {
-        pathLength: {
-          duration: 2
-        },
-        opacity: {
-          duration: 2.5
-        }
-      }
-    },
-    visible: {
-        opacity: 1,
-        pathLength: 1,
-        strokeWidth: '20px',
-        transition: {
-          pathLength: { 
-            type: "spring", 
-            duration: 2, 
-            bounce: 0 
-          },
-          opacity: { 
-            duration: 2 }
-        }
-    },
-    fade: {
-      opacity: 0,
-      pathLength: 1,
-      strokeWidth: '1px',
-      transition: {
-          opacity: {
-              duration: 2
-          }
-      }
+
+const svgPathAnimations = {
+  hidden: { 
+    opacity:0,
+    strokeWidth: '10px',
+    pathLength: 0,
+    transition: {
+      pathLength: {
+        duration: 2
+      },
+      opacity: {
+        duration: 2.5
+      },
     }
-  };
-  
-  
-  export function AnimatedBorderSvg(props: {
-    viewBox?: string,
-    className: string
-    rendered: boolean,
-    setRendered: React.Dispatch<React.SetStateAction<boolean>>
-  }) {
-    return (
-      <motion.svg
-        viewBox={props.viewBox ?? "0 0 600 600"}
-        className={cn('absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  w-[100%] h-[100%]', props.className)}
-        initial="hidden"
-        animate="visible"
-        onAnimationComplete={() => props.setRendered(true)}
-        exit="fade"
-      >
-        <motion.rect
-          className="w-full h-full"
-          fill="white"
-          rx="25" 
-          stroke="#e4e4e7"
-          variants={draw}
-        />
-      </motion.svg>
-    )
+  },
+  visible: {
+      opacity: 1,
+      pathLength: 1,
+      strokeWidth: '20px',
+      color: 'white',
+      transition: {
+        pathLength: { 
+          type: "spring", 
+          duration: 3, 
+          bounce: 0 
+        },
+        opacity: { 
+          duration: 2 
+        }
+      }
+  },
+  fade: {
+    opacity: 0,
+    pathLength: 1,
+    strokeWidth: '1px',
+    transition: {
+        opacity: {
+            duration: 2
+        }
+    }
   }
+};
+
+type AnimatedBorderSvgProps = {
+  /**
+   * @default "0 0 600 600"
+   * @description exposed to easily allow the svg border to fit a rectangle if needed.
+  */
+  viewBox?: string,
+  /**
+   * @description mainly exposed to allow for positioning and z-indexing.
+  */
+  className: string
+  /**
+   * @description required function to notify parent when rendering was completed.
+  */
+  setRendered: React.Dispatch<React.SetStateAction<boolean>>,
+  /**
+   * @description sets type of exit animation. if true svg will undraw itself. 
+  */
+  undrawSVG?: boolean,
+}
+  
+/**
+ * 
+ * @description renders an svg path that deliniates its parents bounds. svg path is drawn and then exits by fading or undrawing.
+ * meant to fade upon completion because svg borders cant be properly rounded on a transparent background.
+ */
+export function AnimatedBorderSvg(props: AnimatedBorderSvgProps) {
+  return (
+    <motion.svg
+      viewBox={props.viewBox ?? "0 0 600 600"}
+      className={cn('absolute top-1/2 left-1/2 transform transition duration-500 -translate-x-1/2 -translate-y-1/2  w-[100%] h-[100%]', props.className)}
+      initial={props.undrawSVG ? "visible" : "hidden"}
+      animate={"visible"}
+      fill={'currentColor'}
+      onAnimationComplete={() => props.setRendered(true)}
+      exit={props.undrawSVG ? "hidden" : "fade"}
+    >
+      <motion.rect
+        className="w-full h-full bg-transparent"
+        rx="25" 
+        stroke={'#e4e4e7'}
+        variants={svgPathAnimations}
+      />
+    </motion.svg>
+  )
+}
--- screenpipe-app-tauri/components/ui/animated-border.tsx
+++ screenpipe-app-tauri/components/ui/animated-border.tsx
@@ -1,40 +1,73 @@
 import { useState } from "react";
 import { AnimatePresence, motion } from 'framer-motion';
-import { AnimatedBorderSvg } from "./animated-border-svg";
+import { AnimatedBorderSvg } from './animated-border-svg';
+import { opacityVisibility } from "@/lib/motion/constants";
 
-const animations = {
-    hidden: { 
-        opacity: 0
-    },
-    visible: { 
-        opacity: 1,
-        transition: {
-            duration: 1,
-        }
-    }
-};
+type AnimatedBorderType = {
+  /**
+   * @default "0 0 600 600"
+   * @description exposed to easily allow the svg border to fit a rectangle if needed.
+  */
+  viewBox?: string
+  /**
+   * @description if true css border will be green. used to highlight component.
+  */
+  showGreenBorder?: boolean
+  /**
+   * @description if true border will be hidden. used to animate/activate border when required.
+  */
+  hidden?: boolean 
 
-export const AnimatedBorder = (props: 
-    {
-        viewBox?: string, 
-        className?: string
+}
+
+/**
+ * @description will render a border that grows into the parent's bounds. it orchestrates the animation of 
+ * an svg path and a css border. svg path makes it look like a line is being drawn around the parent's bounds, 
+ * followed by a slight shine upon completion.
+ */
+export const AnimatedBorder = (props: AnimatedBorderType) => {
+    const [isSVGRendered, setSVGRendered] = useState(false)
+    const [isSVGUnrendered, setSVGUnrendered] = useState(false)
+    const shouldRenderSVG = props.hidden ?  props.showGreenBorder && !isSVGRendered : !isSVGRendered
+    const shouldUndraw = isSVGRendered && props.hidden && !props.showGreenBorder
+    const shouldRenderDiv = isSVGRendered && !shouldUndraw
+    
+    function resetComponent(){
+        setSVGUnrendered(true)
+        setTimeout(()=>{
+            if(props.hidden){
+                setSVGRendered(false)
+                setSVGUnrendered(false)
+            }
+        },1000)
     }
-) => {
-    const [rendered, setRendered] = useState(false)
     return (
         <>
-            <motion.div className="absolute top-0 bg-white z-[10]
-                rounded-lg h-full w-full border-[2px]"
-                initial="hidden"
-                animate={rendered ? 'visible' : undefined}
-                variants={animations}
-            />
             <AnimatePresence>
-                { !rendered && 
+                {shouldRenderDiv && 
+                    <motion.div 
+                        className="absolute top-0 z-[10] rounded-[6px] h-full border-[2px] border-input transition duration-500 w-full data-[isactive=true]:bg-white data-[isactive=true]:border-[#cece66] data-[isactive=true]:border-[2px]"
+                        data-isactive={props.showGreenBorder}
+                        initial="hidden"
+                        animate={'visible'}
+                        exit='hidden'
+                        variants={opacityVisibility}
+                    />
+                }
+                {shouldRenderSVG &&
                     <AnimatedBorderSvg
-                        rendered={rendered}
-                        setRendered={setRendered}
-                        className="rounded-sm"
+                        key={'draw'}
+                        setRendered={setSVGRendered}
+                        className="rounded-[6px] z-[10]"
+                        viewBox={props.viewBox}
+                    />
+                } 
+                {(!isSVGUnrendered && shouldUndraw) &&
+                    <AnimatedBorderSvg
+                        key={'undraw'}
+                        undrawSVG
+                        setRendered={()=>resetComponent()}
+                        className="rounded-[6px]"
                         viewBox={props.viewBox}
                     />
                 }
