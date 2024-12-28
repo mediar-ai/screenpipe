e924eab16c52f17092d20e2ede7c206cd2cd80f1
oliverqx <8559757+oliverqx@users.noreply.github.com>
2024-12-28 16:48:20 -0600

    feat(components): clean typing animation implementation
    - implement better interface
    - improve state management

--- screenpipe-app-tauri/components/ui/typing-animation.tsx
+++ screenpipe-app-tauri/components/ui/typing-animation.tsx
@@ -1,49 +1,86 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useImperativeHandle, useState } from "react";
 
 import { cn } from "@/lib/utils";
 
 interface TypingAnimationProps {
-  text: string;
-  duration?: number;
   className?: string;
-  isDone:() => void;
+  isAnimationComplete(): void;
+  handle: RefObject<TypingAnimationHandle | null> ;
 }
 
-export default function TypingAnimation({
-  text,
-  duration = 200,
+/**
+ * @description handle exposes a function to configure typing animation imperatively.
+ */
+export type TypingAnimationHandle = {
+  /**
+   * @param text - text to type. required argument.
+   * @param duration - duration between character render in ms. defaults to 200.
+   * @description function configures typing animation imperatively.
+   */
+  configureTypingAnimation(args: {text: string, duration: number}): void
+}
+
+/**
+ * @param className - h1 text styling.
+ * @param isAnimationComplete - function called upon animation completion.
+ * @param handle - react ref object that exposes a function to set/reset text to be typed.
+ * @returns h1 react component that triggers a typing animation which can be configured by parent through its handle refrence object.
+ */
+export function TypingAnimation({
+  handle, 
   className,
-  isDone
+  isAnimationComplete,
 }: TypingAnimationProps) {
   const [displayedText, setDisplayedText] = useState<string>("");
-  const [i, setI] = useState<number>(0);
+  const [{textToType, duration}, setAnimationConfiguration] = useState<{textToType: string, duration: number}>({
+    textToType: '',
+    duration: 200
+  })
 
   useEffect(() => {
-    const typingEffect = setInterval(() => {
-      if (i < text.length) {
-        setDisplayedText(text.substring(0, i + 1));
-        setI(i + 1);
-      } else {
-        isDone()
-        clearInterval(typingEffect);
-      }
-    }, duration);
+    if (textToType && displayedText.length < textToType.length) {
+        setTimeout(() => {
+          setDisplayedText(
+            textToType.slice(0, displayedText.length + 1)
+          )
+        }
+      , duration)
+    } else if (textToType) {
+        isAnimationComplete()
+    }
 
-    return () => {
-      clearInterval(typingEffect);
-    };
-  }, [duration, i]);
+  }, [textToType, displayedText]);
+
+  function configureTypingAnimation({
+    text,
+    duration
+  }:{
+    text: string,
+    duration?: number
+  }){
+    setDisplayedText("")
+    setAnimationConfiguration((currentConfig) => {
+      return {
+        textToType: text,
+        duration: typeof duration === 'undefined' ? currentConfig.duration : duration
+      }
+    })
+  }
+
+  useImperativeHandle(handle, () => ({
+    configureTypingAnimation
+  }))
 
   return (
     <h1
       className={cn(
-        "font-display text-center text-4xl font-bold leading-[5rem] tracking-[-0.02em] drop-shadow-sm",
+        "font-display transition-height duration-1000 text-center text-4xl font-bold leading-[5rem] tracking-[-0.02em] drop-shadow-sm",
         className,
       )}
     >
-      {displayedText ? displayedText : text}
+      {displayedText}
     </h1>
   );
 }
