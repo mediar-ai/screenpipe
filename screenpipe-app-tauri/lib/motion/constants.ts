a0d5d8d384becda3db728907f21ad9407195cf27
oliverqx <8559757+oliverqx@users.noreply.github.com>
2024-12-28 16:44:26 -0600

    feat(motion): update constants

--- screenpipe-app-tauri/lib/motion/constants.ts
+++ screenpipe-app-tauri/lib/motion/constants.ts
@@ -9,3 +9,51 @@ export const opacityVisibility = {
     }
   }
 }
+
+export const scaleAnimation = {
+  default: { 
+      scale: 1,
+      transition: {
+          duration: 1,
+      }
+  },
+  shrinkExpand: {
+    scale: [1, 0.95, 1.2],
+    transition: { 
+      duration: 1.2,
+      times: [0, 0.2, 1]
+    },
+  },
+};
+
+export const fadeAnimation = {
+  default: { 
+      opacity: 1,
+      transition: {
+          duration: 1,
+      }
+  },
+  fade: { 
+      opacity: 0,
+      transition: {
+          duration: 1,
+      }
+  }
+};
+
+export const introAnimation = {
+  hidden: {
+      scale: 0.95,
+      opacity: 0,
+      transition: {
+          duration: 0.5,
+      }
+  },
+  visible: { 
+      scale: 1,
+      opacity: 1,
+      transition: {
+          duration: 1,
+      }
+  }
+};
