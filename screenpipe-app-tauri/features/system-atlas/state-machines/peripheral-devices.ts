cbcd1a93d557fd3bf1be3092e7c232773dfd7776
oliverqx <8559757+oliverqx@users.noreply.github.com>
2024-12-30 17:47:06 -0600

    feat(peripheral-devices): improve management state machine
    - moved state machine to module adapters
    - dried code and synced state with permissionState entity/type

--- screenpipe-app-tauri/features/system-atlas/state-machines/peripheral-devices.ts
+++ screenpipe-app-tauri/features/system-atlas/state-machines/peripheral-devices.ts
@@ -1,169 +1,298 @@
-import { ActorRefFrom, fromPromise, sendTo, setup } from "xstate";
-import { AvailablePeripheralDevices } from '../peripheral-devices/data-transfer-objects/available-devices';
+import { ActorRefFrom, assign, fromPromise, sendTo, setup } from "xstate";
+import { AvailablePeripheralDevices, AvailablePeripheralDevicesEnum } from '@/modules/peripheral-devices/types/available-devices'
+import { requestAccessUseCase } from "@/modules/peripheral-devices";
+import checkPermissionStateUseCase from "@/modules/peripheral-devices/use-cases/check-permission-state.use-case";
+import { OSPermissionsStatesPerDevice } from "@/modules/peripheral-devices/types/permission-state-per-device";
 
-let isActive=false
-const checkPermissions = (device: AvailablePeripheralDevices) =>{
-    return  new Promise<boolean>((resolve) => {
-        setTimeout(() => {
-            resolve(isActive)
-        }, 2000);
-    })
-}
+const checkPermission = fromPromise(async ({input}: {input: {device: AvailablePeripheralDevicesEnum}}) => {
+    const permissionsState = await checkPermissionStateUseCase()
 
-const requestAccess = (device: string) =>{
-    window.alert('granting permission to access: '+device)
-    isActive=true
-    return  new Promise<boolean>((resolve) => {
-        setTimeout(() => {
-            resolve(false)
-        }, 2000);
-    })
-}
+    // TODO: 
+    // 1. Either create presenter or rs function to check one device only.
+    // 2. Dirty state management. should sync PermissionState with state machine states.
+    return  permissionsState[input.device]
+})
+
+const requestAccess = fromPromise(async ({input}: {input: {device:AvailablePeripheralDevicesEnum}}) => {
+    await requestAccessUseCase(input.device)
+})
 
 const peripheralDevicesMachine = setup({
     types: {
+        events: {} as { 
+            type: "CHECK" | "REQUEST" | "PENDING" | "SKIP",
+            device: AvailablePeripheralDevicesEnum
+        },
+        context: {} as {
+            activeDevice: AvailablePeripheralDevicesEnum,
+            permissionStatesPerDevice: OSPermissionsStatesPerDevice
+        }
     },
     actors: {
-        checkPermission: fromPromise(async ({input}: {input: {type:string}}) => {
-            const response = await checkPermissions(input.type)
-            return response
-        }),
-        requestAccess: fromPromise(async ({input}: {input: {type:string}}) => {
-            const response = await requestAccess(input.type)
-            return response
-        }),
+        checkPermission,
+        requestAccess 
     }
 }).createMachine({
     description: 'state machine that keeps status and manages input device access.',
-    type: 'parallel',
-    states: {
-        mic: {
-            initial: 'idle',
-            states: {
-                idle: {},
-                pending: {},
-                checking: {
-                    invoke: {
-                        src: 'checkPermission',
-                        input: { type: 'mic' },
-                        onDone: [
-                            {
-                                guard: ({event}) => event.output,
-                                target: 'healthy',
-                            },
-                            {
-                                target: 'unhealthy'
-                            }
-                        ]
-                    }
-                },
-                healthy: {
-                    entry: [
-                        sendTo(({ system }) => system.get('orchestrator'), {type: "HEALTHY"}),
-                    ]
-                },
-                unhealthy: {},
-                requesting : {
-                    invoke: {
-                        src: 'requestAccess',
-                        input: { type: 'mic' },
-                        onDone: 'checking'
-                    }
-                },
-                skipped: {}
-            },
-            on: {
-                "CHECK_MIC": ".checking",
-                "REQUEST_MIC": ".requesting",
-                "PENDING_MIC": ".pending",
-                "SKIP_MIC": ".skipped"
-            }
-        },
-        monitor: {
-            initial: 'idle',
-            states: {
-                idle: {},
-                pending: {},
-                checking: {
-                    invoke: {
-                        src: 'checkPermission',
-                        input: { type: 'monitor' },
-                        onDone: [
-                            {
-                                guard: ({event}) => event.output,
-                                target: 'healthy',
-                            },
-                            {
-                                target: 'unhealthy'
-                            }
-                        ]
-                    }
-                },
-                healthy: {
-                    entry: [
-                        sendTo(({ system }) => system.get('orchestrator'), {type: "HEALTHY"}),
-                    ]
-                },
-                unhealthy: {},
-                requesting : {
-                    invoke: {
-                        src: 'requestAccess',
-                        input: { type: 'monitor' },
-                        onDone: 'checking'
-                    }
-                },
-                skipped: {}
-            },
-            on: {
-                "CHECK_MONITOR": ".checking",
-                "REQUEST_MONITOR": ".requesting",
-                "PENDING_MONITOR": ".pending",
-                "SKIP_MONITOR": ".skipped"
-            }
-        },
-        accessibility: {
-            initial: 'idle',
-            states: {
-                idle: {},
-                pending: {},
-                checking: {
-                    invoke: {
-                        src: 'checkPermission',
-                        input: { type: 'accessibility' },
-                        onDone: [
-                            {
-                                guard: ({event}) => event.output,
-                                target: 'healthy',
-                            },
-                            {
-                                target: 'unhealthy'
-                            }
-                        ]
-                    }
-                },
-                healthy: {
-                    entry: [
-                        sendTo(({ system }) => system.get('orchestrator'), {type: "HEALTHY"}),
-                    ]
-                },
-                unhealthy: {},
-                requesting : {
-                    invoke: {
-                        src: 'requestAccess',
-                        input: { type: 'accessibility' },
-                        onDone: 'checking'
-                    }
-                },
-                skipped: {}
-            },
-            on: {
-                "PENDING_ACCESSIBILITY": ".pending",
-                "REQUEST_ACCESSIBILITY": ".requesting",
-                "CHECK_ACCESSIBILITY": ".checking",
-                "SKIP_ACCESSIBILITY": ".skipped"
-            }
+    context: {
+        activeDevice: AvailablePeripheralDevices.microphone,
+        permissionStatesPerDevice: {
+            'accessibility': 'empty',
+            'microphone': 'empty',
+            'screenRecording': 'empty'
         }
     },
+    initial: 'idle',
+    states: {
+        idle: {},
+        checking: {
+            invoke: {
+                src: 'checkPermission',
+                input: ({context}) => { return { device: context.activeDevice }},
+                onDone: [
+                    {
+                      target: 'idle',
+                      actions: [
+                            ({event, system}) => {
+                                system.get('orchestrator').send({type: event.output.toUpperCase()})
+                            },
+                            assign({
+                                permissionStatesPerDevice: ({event, context}) => {
+                                    // make copy to avoid context object mutation
+                                    const newPermissionStatesPerDevice = { ...context.permissionStatesPerDevice }
+                                    newPermissionStatesPerDevice[context.activeDevice] = event.output
+
+                                    return {
+                                        ...newPermissionStatesPerDevice,
+                                    }   
+                                }
+                            }),
+                        ]
+                    },
+                ]
+            }
+        },
+        requesting : {
+            invoke: {
+                src: 'requestAccess',
+                input: ({context}) => {return { device: context.activeDevice }},
+                onDone: 'checking'
+            }
+        },
+    },
+    on: {
+        "CHECK": {
+            actions: assign({
+                activeDevice: ({ event }) => event.device,
+                permissionStatesPerDevice: ({context, event}) => {
+                    // make copy to avoid context object mutation
+                    const newPermissionStatesPerDevice = { ...context.permissionStatesPerDevice }
+                    newPermissionStatesPerDevice[event.device] = 'pending'
+
+                    return {
+                        ...newPermissionStatesPerDevice,
+                    }   
+                }
+            }),
+            target: ".checking"
+        },
+        "REQUEST": {
+            actions: assign({
+                activeDevice: ({ event }) => event.device,
+                permissionStatesPerDevice: ({context, event}) => {
+                    // make copy to avoid context object mutation
+                    const newPermissionStatesPerDevice = { ...context.permissionStatesPerDevice }
+                    newPermissionStatesPerDevice[event.device] = 'pending'
+
+                    return {
+                        ...newPermissionStatesPerDevice,
+                    }   
+                }
+            }),
+            target: ".requesting",
+        },
+        "PENDING": {
+            actions: assign({
+                activeDevice: ({ event }) => event.device,
+                permissionStatesPerDevice: ({context, event}) => {
+                    // make copy to avoid context object mutation
+                    const newPermissionStatesPerDevice = { ...context.permissionStatesPerDevice }
+                    newPermissionStatesPerDevice[event.device] = 'pending'
+
+                    return {
+                        ...newPermissionStatesPerDevice,
+                    }   
+                }
+            }),
+        },
+        "SKIP": {
+            actions: assign({
+                permissionStatesPerDevice: ({context, event}) => {
+                    // make copy to avoid context object mutation
+                    const newPermissionStatesPerDevice = { ...context.permissionStatesPerDevice }
+                    newPermissionStatesPerDevice[event.device] = 'skipped'
+
+                    return {
+                        ...newPermissionStatesPerDevice,
+                    }   
+                }
+            }),
+        }
+    }
+    // {
+    //     mic: {
+    //         initial: 'empty',
+    //         states: {
+    //             empty: {},
+    //             pending: {},
+    //             notNeeded: {},
+    //             granted: {
+    //                 entry: [
+    //                     sendTo(({ system }) => system.get('orchestrator'), {type: "GRANTED"}),
+    //                 ]
+    //             },
+    //             denied: {
+    //                 entry: [
+    //                     sendTo(({ system }) => system.get('orchestrator'), {type: "DENIED"}),
+    //                 ]
+    //             },
+    //             skipped: {},
+    //             checking: {
+    //                 invoke: {
+    //                     src: 'checkPermission',
+    //                     input: { device: AvailablePeripheralDevices.microphone },
+    //                     onDone: [
+    //                         {
+    //                             guard: ({event}) => event.output === 'empty',
+    //                             target: 'empty'
+    //                         },
+    //                         {
+    //                             guard: ({event}) => event.output === 'denied',
+    //                             target: 'denied'
+    //                         },
+    //                         {
+    //                             guard: ({event}) => event.output === 'granted',
+    //                             target: 'granted'
+    //                         },
+    //                         {
+    //                             guard: ({event}) => event.output === 'notNeeded',
+    //                             target: 'notNeeded'
+    //                         }
+    //                     ]
+    //                 }
+    //             },
+    //             requesting : {
+    //                 invoke: {
+    //                     src: 'requestAccess',
+    //                     input: { device: AvailablePeripheralDevices.microphone },
+    //                     onDone: 'checking'
+    //                 }
+    //             },
+    //         },
+    //         on: {
+    //             "CHECK": {
+    //                 guard: ({event}) => event.device === AvailablePeripheralDevices.microphone,
+    //                 target: ".checking"
+    //             },
+    //             "REQUEST": {
+    //                 guard: ({event}) => event.device === AvailablePeripheralDevices.microphone,
+    //                 target: ".requesting",
+    //             },
+    //             "PENDING": {
+    //                 guard: ({event}) => event.device === AvailablePeripheralDevices.microphone,
+    //                 target: ".pending"
+    //             },
+    //             "SKIP": {
+    //                 guard: ({event}) => event.device === AvailablePeripheralDevices.microphone,
+    //                 target: ".skipped"
+    //             }
+    //         }
+    //     },
+        // monitor: {
+        //     initial: 'idle',
+        //     states: {
+        //         idle: {},
+        //         pending: {},
+        //         checking: {
+        //             invoke: {
+        //                 src: 'checkPermission',
+        //                 input: { device: AvailablePeripheralDevices.screenRecording },
+        //                 onDone: [
+        //                     {
+        //                         guard: ({event}) => event.output,
+        //                         target: 'healthy',
+        //                     },
+        //                     {
+        //                         target: 'unhealthy'
+        //                     }
+        //                 ]
+        //             }
+        //         },
+        //         healthy: {
+        //             entry: [
+        //                 sendTo(({ system }) => system.get('orchestrator'), {type: "HEALTHY"}),
+        //             ]
+        //         },
+        //         unhealthy: {},
+        //         requesting : {
+        //             invoke: {
+        //                 src: 'requestAccess',
+        //                 input: { device: AvailablePeripheralDevices.screenRecording },
+        //                 onDone: 'checking'
+        //             }
+        //         },
+        //         skipped: {}
+        //     },
+        //     on: {
+        //         "CHECK_MONITOR": ".checking",
+        //         "REQUEST_MONITOR": ".requesting",
+        //         "PENDING_MONITOR": ".pending",
+        //         "SKIP_MONITOR": ".skipped"
+        //     }
+        // },
+        // accessibility: {
+        //     initial: 'idle',
+        //     states: {
+        //         idle: {},
+        //         pending: {},
+        //         checking: {
+        //             invoke: {
+        //                 src: 'checkPermission',
+        //                 input: { device: AvailablePeripheralDevices.accessibility },
+        //                 onDone: [
+        //                     {
+        //                         guard: ({event}) => event.output,
+        //                         target: 'healthy',
+        //                     },
+        //                     {
+        //                         target: 'unhealthy'
+        //                     }
+        //                 ]
+        //             }
+        //         },
+        //         healthy: {
+        //             entry: [
+        //                 sendTo(({ system }) => system.get('orchestrator'), {type: "HEALTHY"}),
+        //             ]
+        //         },
+        //         unhealthy: {},
+        //         requesting : {
+        //             invoke: {
+        //                 src: 'requestAccess',
+        //                 input: { device: AvailablePeripheralDevices.accessibility },
+        //                 onDone: 'checking'
+        //             }
+        //         },
+        //         skipped: {}
+        //     },
+        //     on: {
+        //         "PENDING_ACCESSIBILITY": ".pending",
+        //         "REQUEST_ACCESSIBILITY": ".requesting",
+        //         "CHECK_ACCESSIBILITY": ".checking",
+        //         "SKIP_ACCESSIBILITY": ".skipped"
+        //     }
+        // }
+    // },
 });
 
 export type PeripheralDevicesMachineType = ActorRefFrom<typeof peripheralDevicesMachine>
