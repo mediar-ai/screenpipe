0acb1290518e94c1093b4cdf211f5aef6cb0090c
neo773 <62795688+neo773@users.noreply.github.com>
2024-12-30 22:49:43 +0530

    Swap configs (#1050)
    * Committing any uncommitted changes
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * revert
    
    * revert
    
    * wip
    
    * wip
    
    * wip
    
    * finally fix bug
    
    * wip
    
    * fix
    
    * add warning
    
    * fix conflict
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * changes
    
    * fix
    
    * fix
    
    * fix profile_shortcuts
    
    * fix profile_shortcuts
    
    * fix updateShortcut

--- screenpipe-app-tauri/app/providers.tsx
+++ screenpipe-app-tauri/app/providers.tsx
@@ -7,32 +7,36 @@ import { initOpenTelemetry } from "@/lib/opentelemetry";
 import { OnboardingProvider } from "@/lib/hooks/use-onboarding";
 import { ChangelogDialogProvider } from "@/lib/hooks/use-changelog-dialog";
 import { forwardRef } from "react";
-import { store } from "@/lib/hooks/use-settings";
-import { StoreProvider } from "easy-peasy";
+import { store as SettingsStore } from "@/lib/hooks/use-settings";
+import { profilesStore as ProfilesStore } from "@/lib/hooks/use-profiles";
 
-export const Providers = forwardRef<HTMLDivElement, { children: React.ReactNode }>(
-  ({ children }, ref) => {
-    useEffect(() => {
-      if (typeof window !== "undefined") {
-        const isDebug = process.env.TAURI_ENV_DEBUG === "true";
-        if (isDebug) return;
-        posthog.init("phc_Bt8GoTBPgkCpDrbaIZzJIEYt0CrJjhBiuLaBck1clce", {
-          api_host: "https://eu.i.posthog.com",
-          person_profiles: "identified_only",
-          capture_pageview: false,
-        });
-        initOpenTelemetry("82688", new Date().toISOString());
-      }
-    }, []);
+export const Providers = forwardRef<
+  HTMLDivElement,
+  { children: React.ReactNode }
+>(({ children }, ref) => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const isDebug = process.env.TAURI_ENV_DEBUG === "true";
+      if (isDebug) return;
+      posthog.init("phc_Bt8GoTBPgkCpDrbaIZzJIEYt0CrJjhBiuLaBck1clce", {
+        api_host: "https://eu.i.posthog.com",
+        person_profiles: "identified_only",
+        capture_pageview: false,
+      });
+      initOpenTelemetry("82688", new Date().toISOString());
+    }
+  }, []);
 
   return (
-    <StoreProvider store={store}>
-      <OnboardingProvider>
-        <ChangelogDialogProvider>
-          <PostHogProvider client={posthog}>{children}</PostHogProvider>
-        </ChangelogDialogProvider>
-      </OnboardingProvider>
-    </StoreProvider>
+    <SettingsStore.Provider>
+      <ProfilesStore.Provider>
+        <OnboardingProvider>
+          <ChangelogDialogProvider>
+            <PostHogProvider client={posthog}>{children}</PostHogProvider>
+          </ChangelogDialogProvider>
+        </OnboardingProvider>
+      </ProfilesStore.Provider>
+    </SettingsStore.Provider>
   );
 });
 
