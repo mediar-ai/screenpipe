c6087f7ac2b9004ac715e6a9d6f627a53b1c4eb8
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-01 11:07:59 -0600

    feat(screenpipe-cli): add spawn function to cli service
    - update service interface
    - update tauri service

--- screenpipe-app-tauri/modules/screenpipe-cli/infrastructure/cli.tauri.service.ts
+++ screenpipe-app-tauri/modules/screenpipe-cli/infrastructure/cli.tauri.service.ts
@@ -2,6 +2,7 @@ import { Command } from "@tauri-apps/plugin-shell";
 import { ScreenpipeSetupParams } from "../types/screenpipe-setup-params";
 import { ScreenpipeCliService } from "../interfaces/cli.service.interface";
 import { EventEmitter } from "@/modules/event-management/emitter/interfaces/event-emitter.service.interface";
+import { invoke } from "@tauri-apps/api/core";
 
 class TauriCliService implements ScreenpipeCliService {
     constructor(
@@ -9,32 +10,41 @@ class TauriCliService implements ScreenpipeCliService {
     ) {}
     
     async setup({enableBeta = false}: ScreenpipeSetupParams) {
-          const command = Command.sidecar("screenpipe", getSetupParams(enableBeta));
-    
-          const timeoutPromise = timeout(900000, "setup timed out");
-          const outputPromise = new Promise<string>((resolve, reject) => {
-            command.on("close", (data) => {
-              if (data.code !== 0) {
-                reject(new Error(`command failed with code ${data.code}`));
-              }
-            });
-            command.on("error", (error) => reject(new Error(error)));
-            
-            command.stdout.on("data", (line) => {
-              this.eventEmitterService.emit('command-update', line)
-              if (line.includes("screenpipe setup complete")) {
-                resolve("ok");
-              }
-            });
-          });
-    
-          try {
-            await command.spawn();
-            await Promise.race([outputPromise, timeoutPromise]);
-          } catch (error) {
-            console.error("error or timeout:", error);
+      const command = Command.sidecar("screenpipe", getSetupParams(enableBeta));
+
+      const timeoutPromise = timeout(900000, "setup timed out");
+      const outputPromise = new Promise<string>((resolve, reject) => {
+        command.on("close", (data) => {
+          if (data.code !== 0) {
+            reject(new Error(`command failed with code ${data.code}`));
           }
-      };
+        });
+        command.on("error", (error) => reject(new Error(error)));
+        
+        command.stdout.on("data", (line) => {
+          this.eventEmitterService.emit('command-update', line)
+          if (line.includes("screenpipe setup complete")) {
+            resolve("ok");
+          }
+        });
+      });
+
+      try {
+        await command.spawn();
+        await Promise.race([outputPromise, timeoutPromise]);
+      } catch (error) {
+        console.error("error or timeout:", error);
+      }
+    };
+
+    async spawn(): Promise<void> {
+      try {
+        await invoke("spawn_screenpipe");
+        console.log("Command executed successfully");
+      } catch (error) {
+        console.error("Command failed:", error);
+      }
+    }
 }
 
 export default TauriCliService
--- screenpipe-app-tauri/modules/screenpipe-cli/interfaces/cli.service.interface.ts
+++ screenpipe-app-tauri/modules/screenpipe-cli/interfaces/cli.service.interface.ts
@@ -2,4 +2,5 @@ import { ScreenpipeSetupParams } from "../types/screenpipe-setup-params";
 
 export interface ScreenpipeCliService {
     setup(params: ScreenpipeSetupParams): void,
+    spawn(): Promise<void>
 }
