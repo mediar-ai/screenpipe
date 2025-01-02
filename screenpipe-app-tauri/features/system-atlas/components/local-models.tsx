fcbdd5e9d39ffe9790e14201c2848a6377b57776
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-02 12:07:15 -0600

    feat(system-atlas): add hover card to models
    - will display information on hover

--- screenpipe-app-tauri/features/system-atlas/components/local-models.tsx
+++ screenpipe-app-tauri/features/system-atlas/components/local-models.tsx
@@ -5,6 +5,7 @@ import { AnimatedGroupContainer } from "@/components/ui/animated-group-container
 import { CircleIcon } from "@/components/ui/circle-icon"
 import WhisperIcon from "@/components/icons/whisper-huggingface"
 import OnnxGithubIcon from "@/components/icons/onnx-github"
+import { HoverCard, HoverCardContent, HoverCardInfo, HoverCardTrigger } from "@/components/ui/hover-card"
 
 const LocalModels = (props: {
     llmModelsRef: Ref<HTMLDivElement> | null,
@@ -24,16 +25,29 @@ const LocalModels = (props: {
                 ref={props.llmModelsRef} 
                 className={'bg-white rounded-lg h-[65px] w-[130px] flex flex-row justify-around'}
             >
-                <CircleIcon
-                    state={deviceStates.openai}
+
+                <HoverCardInfo
+                    title="openai's whisper model (stt)"
+                    description="robust speech recognition via large-scale weak supervision."
+                    footer="will be downloaded from hugging face."
                 >
-                    <WhisperIcon/>
-                </CircleIcon>
-                <CircleIcon
-                    state={deviceStates.llama}
+                    <CircleIcon
+                        state={deviceStates.openai}
+                    >
+                        <WhisperIcon/>
+                    </CircleIcon>
+                </HoverCardInfo>
+                <HoverCardInfo
+                    title="snakers4's silero in (vad)"
+                    description="voice activity detection tool using next-gen kaldi with onnxruntime without internet connection."
+                    footer="will be downloaded from github."
                 >
-                    <OnnxGithubIcon/>
-                </CircleIcon>
+                    <CircleIcon
+                        state={deviceStates.llama}
+                    >
+                        <OnnxGithubIcon/>
+                    </CircleIcon>
+                </HoverCardInfo>
             </AnimatedGroupContainer>
         </span>
     )
