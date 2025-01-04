c156180ef8db618b386df2a39ca036556242a685
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-04 08:18:02 -0600

    fix(event-management): update useEventListener hook
    - implements new event schema

--- screenpipe-app-tauri/modules/event-management/listener/adapters/react/hook.ts
+++ screenpipe-app-tauri/modules/event-management/listener/adapters/react/hook.ts
@@ -1,25 +1,23 @@
 import { useEffect, useState } from "react";
 import { EventListenerService } from "../../interfaces/event-listener.service.interface";
 import { reactLogPresenter, ReactLogPresenterOutput } from '../../../utils/utils';
+import { ScreenpipeAppEvent } from "@/modules/event-management/emitter/interfaces/event-emitter.service.interface";
 
 export function useEventListener(event: string, listener: EventListenerService) {
     const [data, setData] = useState<ReactLogPresenterOutput | null>(null);
 
     useEffect(() => {
-        // Listener function that updates state with the event data
-        const handleEventCallback = (eventData: any) => {
-            const parsedEvent = reactLogPresenter(eventData)
+        const handleEventCallback = (eventData: ScreenpipeAppEvent) => {
+            const parsedEvent = reactLogPresenter(eventData.detail)
             setData(parsedEvent);
         };
 
-        // Subscribe to the event
         listener.on(event, handleEventCallback);
 
-        // Cleanup listener when the component unmounts
         return () => {
             listener.off(event);
         };
-    }, [event, listener]); // Re-run effect if the event or emitter changes
+    }, [event, listener]);
 
     return data;
 }
