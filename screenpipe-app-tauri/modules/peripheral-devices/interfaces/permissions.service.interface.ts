79f91f8daa7002bb2d1ca8e57942f9fcac7f59ac
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-04 18:35:54 -0600

    feat(peripheral-devices): add js doc to permissions service interface

--- screenpipe-app-tauri/modules/peripheral-devices/interfaces/permissions.service.interface.ts
+++ screenpipe-app-tauri/modules/peripheral-devices/interfaces/permissions.service.interface.ts
@@ -1,7 +1,21 @@
 import { AvailablePeripheralDevicesEnum } from "../types/available-devices";
 import { OSPermissionsStatesPerDevice } from "../types/permission-state-per-device";
 
+type RequestPermissionParams = { 
+    /**
+     * @description: string that represents one of the available peripheral devices.
+     */
+    device: AvailablePeripheralDevicesEnum 
+}
+
+/**
+ * @description A service to manage peripheral device permissions.
+ */
 export interface PermissionsService {
-    requestPermission({ device }: {device: AvailablePeripheralDevicesEnum }): Promise<void>;
+    /**
+     * @description requests permission for a specific peripheral device.
+     * @returns {Promise<void>} a promise that resolves once request permission call to os has been made.
+    */
+    requestPermission({ device }: RequestPermissionParams): Promise<void>;
     checkPermissions({ initialCheck } : { initialCheck: boolean }): Promise<OSPermissionsStatesPerDevice>;
 }
