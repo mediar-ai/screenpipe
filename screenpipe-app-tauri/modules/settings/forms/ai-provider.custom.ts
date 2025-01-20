2b72453283c3b556c2810a5bb98240309cb5a1ab
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-20 11:09:07 -0600

    feat(forms): add ai provider setup forms

---
+++ screenpipe-app-tauri/modules/settings/forms/ai-provider.custom.ts
@@ -0,0 +1,78 @@
+import { FieldSchema } from "@/modules/form/entities/field/field-metadata"
+
+const fields: FieldSchema[] = [
+  {
+    key: 'endpointUrl',
+    title: 'endpoint url',
+    validationMeta: {
+      errorMessage: 'this field is mandatory',
+      min: 1,
+      max: 50,
+      optional: false
+    },
+    typeMeta: {
+      isRegular: true,
+      type: 'STRING'
+    }
+  },
+  {
+    key: 'aiModel',
+    title: 'ai model',
+    validationMeta: {
+     optional: false,
+     errorMessage: 'hey' 
+    },
+    typeMeta: {
+      isRegular: false,
+      type: 'SELECT', 
+      options: [
+        {
+          value: "gpt-4o",
+          label: "gpt-4o"
+        },
+        {
+          value: "gpt-4o-mini",
+          label: "gpt-4o-mini"
+        },
+        {
+          value: "o1-mini",
+          label: "o1-mini",
+        },
+        {
+          value: "o1",
+          label: "o1"
+        },
+      ]
+    }
+  },
+  {
+    key: 'prompt',
+    title: 'prompt',
+    validationMeta: {
+      optional: false,
+      errorMessage: 'you need to provide a custom prompt'
+    },
+    typeMeta: {
+      isRegular: true,
+      type: 'TEXTAREA'
+    }
+  },
+  {
+    key: 'maxContent',
+    title: 'max content',
+    validationMeta: {
+      optional: false,
+      errorMessage: 'you need to provide a custom prompt'
+    },
+    typeMeta: {
+      isRegular: true,
+      type: 'SLIDER'
+    }
+  }
+]
+
+export const CustomSetupForm = {
+  title: 'openai provider setup',
+  fields,
+  buttonText: 'submit changes',
+}
---
+++ screenpipe-app-tauri/modules/settings/forms/ai-provider.screenpipe-cloud.ts
@@ -0,0 +1,92 @@
+import { FieldSchema } from "@/modules/form/entities/field/field-metadata"
+
+const fields: FieldSchema[] = [
+  {
+    key: 'aiModel',
+    title: 'ai model',
+    validationMeta: {
+     optional: false,
+     errorMessage: 'hey' 
+    },
+    typeMeta: {
+      isRegular: false,
+      type: 'SELECT',
+      options: [
+        {
+          value: "gpt-4o",
+          label: "gpt-4o"
+        },
+        {
+          value: "gpt-4o-mini",
+          label: "gpt-4o-mini"
+        },
+        {
+          value: "o1-mini",
+          label: "o1-mini",
+        },
+        {
+          value: "o1",
+          label: "o1"
+        },
+        {
+          value: "claude-3-5-sonnet-latest",
+          label: "claude-3-5-sonnet-latest",
+        },
+        {
+          value: "claude-3-5-haiku-latest",
+          label: "claude-3-5-haiku-latest",
+        },
+        {
+          value: "gemini-2.0-flash-exp",
+          label: "gemini-2.0-flash-exp",
+        },
+        {
+          value: "gemini-2.0-flash-exp",
+          label: "gemini-2.0-flash-exp",
+        },
+        {
+          value: "gemini-1.5-flash",
+          label: "gemini-1.5-flash",
+        },
+        {
+          value: "gemini-1.5-flash-8b",
+          label: "gemini-1.5-flash-8b"
+        },
+        {
+          value: "gemini-1.5-pro",
+          label: "gemini-1.5-pro"
+        }
+      ]
+    }
+  },
+  {
+    key: 'prompt',
+    title: 'prompt',
+    validationMeta: {
+      optional: false,
+      errorMessage: 'you need to provide a custom prompt'
+    },
+    typeMeta: {
+      isRegular: true,
+      type: 'TEXTAREA'
+    }
+  },
+  {
+    key: 'maxContent',
+    title: 'max content',
+    validationMeta: {
+      optional: false,
+      errorMessage: 'you need to provide a custom prompt'
+    },
+    typeMeta: {
+      isRegular: true,
+      type: 'SLIDER'
+    }
+  }
+]
+
+export const ScreenpipeCloudSetupForm = {
+  title: 'screenpipe cloud provider setup',
+  fields,
+  buttonText: 'submit changes',
+}
