34aad21f240504ba708af38e708ff7dd17bfb78a
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-01-21 11:12:16 -0600

    feat(ui): add button with loading state

---
+++ screenpipe-app-tauri/components/ui/button-with-loading-state.tsx
@@ -0,0 +1,33 @@
+import { ButtonHTMLAttributes } from "react"
+import Spinner from "./spinner"
+import { Button } from "./button"
+
+export function ButtonWithLoadingState({
+    isLoading,
+    className,
+    handleClick,
+    label,
+    disabled,
+    type
+  } : {
+    label: string,
+    handleClick?: () => void
+    isLoading?: boolean,
+    className?: string,
+    disabled?:boolean,
+    type?: ButtonHTMLAttributes<HTMLButtonElement>['type']
+  }) {
+    return (
+      <Button onClick={handleClick} className={className} disabled={disabled} type={type}> 
+        <div className="flex items-end">
+          <p className="leading-none">
+            {!isLoading ? label : "Cargando..."}
+          </p>
+          {isLoading ?
+          <div className="max-w-[15px] w-[15px] h-[15px] ml-4">
+            <Spinner/>
+          </div> : null}
+        </div>
+     </Button>
+    )
+  }
