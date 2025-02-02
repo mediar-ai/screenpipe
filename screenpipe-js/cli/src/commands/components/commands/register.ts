a9c3338b46ced441008a675c3db235d6e1163db3
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-02-01 18:47:32 -0600

    feat(cli): migrate components commands to Commander

--- screenpipe-js/cli/src/commands/components/commands/register.ts
+++ screenpipe-js/cli/src/commands/components/commands/register.ts
@@ -5,6 +5,7 @@ import prompts from "prompts";
 import { ComponentSchema, RegistrySchema } from "./add/registry/schema";
 import { getRegistry } from "./add/registry/api";
 import fs from 'fs-extra';
+import { Command } from "commander";
 
 async function writeJsonToFile(filePath: string, data: RegistrySchema) {
   try {
@@ -25,98 +26,101 @@ async function writeJsonToFile(filePath: string, data: RegistrySchema) {
   }
 }
 
-export const registerComponentCommand = command({
-    name: "register",
-    desc: "register a new component in screenpipe's component registry",
-    options: {
-      name: string().desc("name of the component"),
-      src: string().desc("github url for the component."),
-      target: string().desc("path where file should be created."),
-    },
-    handler: async (opts) => {
-      try {
-        if (!opts.name) {
-          const { name } = await prompts({
-            type: "text",
-            name: "name",
-            message: "what's your component's name?",
-            instructions: false,
-          })
-
-          opts.name = name
-        }
-
-        if (!opts.src) {
-          const { src } = await prompts({
-            type: "text",
-            name: "src",
-            message: "where should we download the component from?",
-            hint: "url with the following pattern: https://api.github.com/repos/{owner}/{repo}/contents/{path}. see README for more info.",
-          })
-
-          opts.src = src
-        }
-
-        if (!opts.target) {
-          const { target } = await prompts({
-            type: "text",
-            name: "target",
-            message: "where should the component be created?",
-          })
-
-          opts.target = target
-        }
-
-        if (!opts.name?.length || !opts.src?.length || !opts.target?.length) {
-          logger.break()
-          handleError('invalid component')
-          process.exit(1)
-        }
-
-        const { deps } = await prompts({
-          type: "list",
-          name: "deps",
-          message: "type all of the component's runtime dependencies by name, separated by a comma",
-          separator: ',',
+export const registerComponentCommand = new Command()
+  .name("register")
+  .description("register a new component in screenpipe's component registry")
+  .option(
+    "-n, --name <name>",
+    "name of the component"
+  ).option(
+    "-s, --src",
+    "github url for the component"
+  ).option(
+    "-t, --target",
+    "path where file should be created"
+  ).action(async (opts) => {
+    try {
+      if (!opts.name) {
+        const { name } = await prompts({
+          type: "text",
+          name: "name",
+          message: "what's your component's name?",
+          instructions: false,
         })
 
-        const { devDeps } = await prompts({
-          type: "list",
-          name: "devDeps",
-          message: "type all of the component's dev dependencies by name, separated by a comma",
-          separator: ',',
-        })
-
-        const { registryDeps } = await prompts({
-          type: "list",
-          name: "registryDeps",
-          message: "type all of the component's registry dependencies by name, separated by a comma",
-          separator: ',',
-        })
-
-        const componentObject:  ComponentSchema = {
-          name: opts.name as string,
-          src: opts.src as string,
-          target: opts.target as string,
-          dependencies: (deps as string[]).filter(item => item !== ""),
-          devDependencies: (devDeps as string[]).filter(item => item !== ""),
-          registryDependencies: (registryDeps as string[]).filter(item => item !== "")
-        }
-        
-        const currentRegistry = await getRegistry()
-        if (!currentRegistry) {
-          logger.break()
-          handleError('critical: build is missing registry file.')
-          process.exit(1)
-        }
-
-        currentRegistry[opts.name as string] = componentObject
-
-        await writeJsonToFile('./src/commands/components/commands/add/registry/registry.json', currentRegistry)
-        logger.log("run `bun run build` and open a PR at https://github.com/mediar-ai/screenpipe to update registry.")
-      } catch (error) {
-        logger.break()
-        handleError(error)
+        opts.name = name
       }
-    },
+
+      if (!opts.src) {
+        const { src } = await prompts({
+          type: "text",
+          name: "src",
+          message: "where should we download the component from?",
+          hint: "url with the following pattern: https://api.github.com/repos/{owner}/{repo}/contents/{path}. see README for more info.",
+        })
+
+        opts.src = src
+      }
+
+      if (!opts.target) {
+        const { target } = await prompts({
+          type: "text",
+          name: "target",
+          message: "where should the component be created?",
+        })
+
+        opts.target = target
+      }
+
+      if (!opts.name?.length || !opts.src?.length || !opts.target?.length) {
+        logger.break()
+        handleError('invalid component')
+        process.exit(1)
+      }
+
+      const { deps } = await prompts({
+        type: "list",
+        name: "deps",
+        message: "type all of the component's runtime dependencies by name, separated by a comma",
+        separator: ',',
+      })
+
+      const { devDeps } = await prompts({
+        type: "list",
+        name: "devDeps",
+        message: "type all of the component's dev dependencies by name, separated by a comma",
+        separator: ',',
+      })
+
+      const { registryDeps } = await prompts({
+        type: "list",
+        name: "registryDeps",
+        message: "type all of the component's registry dependencies by name, separated by a comma",
+        separator: ',',
+      })
+
+      const componentObject:  ComponentSchema = {
+        name: opts.name as string,
+        src: opts.src as string,
+        target: opts.target as string,
+        dependencies: (deps as string[]).filter(item => item !== ""),
+        devDependencies: (devDeps as string[]).filter(item => item !== ""),
+        registryDependencies: (registryDeps as string[]).filter(item => item !== "")
+      }
+      
+      const currentRegistry = await getRegistry()
+      if (!currentRegistry) {
+        logger.break()
+        handleError('critical: build is missing registry file.')
+        process.exit(1)
+      }
+
+      currentRegistry[opts.name as string] = componentObject
+
+      await writeJsonToFile('./src/commands/components/commands/add/registry/registry.json', currentRegistry)
+      logger.log("run `bun run build` and open a PR at https://github.com/mediar-ai/screenpipe to update registry.")
+    } catch (error) {
+      logger.break()
+      handleError(error)
+    }
   })
--- screenpipe-js/cli/src/index.ts
+++ screenpipe-js/cli/src/index.ts
@@ -1,27 +1,51 @@
-#!/usr/bin/env bun
-import { run } from "@drizzle-team/brocli";
-import {
-  loginCommand,
-  logoutCommand,
-  publishCommand,
-  registerCommand,
-  listVersionsCommand,
-  createCommand,
-  componentsCommands
-} from "./commands";
+// import { run } from "@drizzle-team/brocli";
+// import {
+//   // loginCommand,
+//   // logoutCommand,
+//   // publishCommand,
+//   // registerCommand,
+//   // listVersionsCommand,
+//   // createCommand,
+//   // componentsCommands
+// } from "./commands";
+// import { addComponentCommand } from "./commands/components/commands/add/add";
 
-run(
-  [
-    loginCommand,
-    logoutCommand,
-    createCommand,
-    componentsCommands,
-    registerCommand,
-    publishCommand,
-    listVersionsCommand,
-  ],
-  {
-    name: "screenpipe-dev",
-    description: "screenpipe development CLI tool",
-  }
-);
+import { Command } from "commander";
+import { addComponentCommand } from "./commands/components/commands/add/add";
+import { componentsCommands } from "./commands";
+
+// run(
+//   [
+//     // loginCommand,
+//     // logoutCommand,
+//     // createCommand,
+//     // componentsCommands,
+//     // registerCommand,
+//     // publishCommand,
+//     // listVersionsCommand,
+//     addComponentCommand
+//   ],
+//   {
+//     name: "screenpipe-dev",
+//     description: "screenpipe development CLI tool",
+//   }
+// );
+
+process.on("SIGINT", () => process.exit(0))
+process.on("SIGTERM", () => process.exit(0))
+
+async function main() {
+  const program = new Command();
+
+  program
+    .name('screenpipe-dev')
+    .description('screenpipe development CLI tool')
+    .version('0.0.1');
+
+  program
+    .addCommand(componentsCommands)
+
+  program.parse()
+}
+
+main()
