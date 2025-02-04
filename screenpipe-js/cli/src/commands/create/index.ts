bd645a54ce942b4773f6aabfb457f7ae58938b71
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-02-03 20:21:39 -0600

    feat(cli): add create app command
    - tauri
    - electron

--- screenpipe-js/cli/src/commands/create/index.ts
+++ screenpipe-js/cli/src/commands/create/index.ts
@@ -1,8 +1,11 @@
 import { Command } from "commander";
 import { createPipeCommand } from "./pipe";
+import { createAppCommand } from "./app";
 
 export const createCommands = new Command()
   .name("create")
   .description("commands to initiate projects that contribute to screenpipe")
 
 createCommands.addCommand(createPipeCommand)
+
+createCommands.addCommand(createAppCommand)
