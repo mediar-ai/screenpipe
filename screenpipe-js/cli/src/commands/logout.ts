1707f59e4f8c8f8d49c7002be481b304066ef5be
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-02-03 18:18:43 -0600

    feat(cli): implement logger logout

--- screenpipe-js/cli/src/commands/logout.ts
+++ screenpipe-js/cli/src/commands/logout.ts
@@ -1,12 +1,13 @@
 import { Credentials } from "../utils/credentials";
 import { colors, symbols } from "../utils/colors";
 import { Command } from "commander";
+import { logger } from "./components/commands/add/utils/logger";
 
 export const logoutCommand = new Command()
   .name("logout")
   .description("end current session")
   .action(async () => {
     Credentials.clearCredentials();
-    console.log(colors.success(`\n${symbols.success} Successfully logged out`));
-    console.log(colors.info(`${symbols.info} Thanks for using ScreenPipe! Come back soon.`));
+    logger.success(`\n${symbols.success} successfully logged out`);
+    logger.info(`${symbols.info} thanks for using screenpipe! come back soon.`);
   })
