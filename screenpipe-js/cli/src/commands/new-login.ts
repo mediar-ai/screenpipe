f1e516288cfe7366d36cbd7ba57b3296d6e27289
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-02-06 22:01:46 -0600

    feat(cli): init cli login command

---
+++ screenpipe-js/cli/src/commands/new-login.ts
@@ -0,0 +1,124 @@
+import { Command } from "commander";
+import http from "http";
+import { spawn } from "child_process";
+import { writeFileSync } from "fs";
+import { ParsedUrlQuery } from "node:querystring";
+import url from "url";
+import { listen } from "async-listen";
+import "dotenv/config";
+import { customAlphabet } from "nanoid";
+import { logger } from "./components/commands/add/utils/logger";
+import { colors } from "../utils/colors";
+import { handleError } from "./components/commands/add/utils/handle-error";
+import os from 'os';
+import path from "node:path";
+
+const FILENAME = ".unkey";
+
+class UserCancellationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "UserCancellationError";
+  }
+}
+
+async function writeToConfigFile(data: ParsedUrlQuery) {
+    try {
+      const homeDir = os.homedir();
+      const filePath = path.join(homeDir, FILENAME);
+      writeFileSync(filePath, JSON.stringify(data));
+    } catch (error) {
+      handleError(`Error writing to local config file ${error}`);
+    }
+}
+
+const nanoid = customAlphabet("123456789QAZWSXEDCRFVTGBYHNUJMIKOLP", 8);
+
+export const loginCommand = new Command()
+    .name("login")
+    .description("authenticate with screenpipe")
+    .action(async (...args) => {
+        // need to import ora dynamically since it's ESM-only
+        const oraModule = await import("ora");
+        const ora = oraModule.default;
+
+        // create localhost server for our page to call back to
+        const server = http.createServer();
+        const { port } = await listen(server, 0, "127.0.0.1");
+        
+        logger.info(`Server listening on http://127.0.0.1:${port}`);
+
+        // set up HTTP server that waits for a request containing an API key
+        const authPromise = new Promise<ParsedUrlQuery>((resolve, reject) => {
+            server.on("request", (req, res) => {
+            logger.info(`Received ${req.method} request to ${req.url}`);
+            
+            // Set CORS headers for all responses
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+            if (req.method === "OPTIONS") {
+                res.writeHead(200);
+                res.end();
+            } else if (req.method === "GET") {
+                const parsedUrl = url.parse(req.url as string, true);
+                const queryParams = parsedUrl.query;
+                logger.info('Received query params:', queryParams);
+                
+                if (queryParams.cancelled) {
+                    res.writeHead(200);
+                    res.end("Cancelled");
+                    reject(new UserCancellationError("Login process cancelled by user."));
+                } else {
+                    res.writeHead(200);
+                    res.end("Success");
+                    resolve(queryParams);
+                }
+            } else {
+                res.writeHead(405);
+                res.end("Method not allowed");
+            }
+            });
+        });
+
+        const redirect = `http://127.0.0.1:${port}`;
+
+        const code = nanoid();
+        const confirmationUrl = new URL(`${process.env.CLIENT_URL}/auth/devices`);
+        confirmationUrl.searchParams.append("code", code);
+        confirmationUrl.searchParams.append("redirect", redirect);
+        logger.log(`Confirmation code: ${colors.bold(code)}\n`);
+        logger.log(
+            `If something goes wrong, copy and paste this URL into your browser: ${colors.bold(
+            confirmationUrl.toString(),
+            )}\n`,
+        );
+        spawn("open", [confirmationUrl.toString()]);
+        const spinner = ora("Waiting for authentication...\n\n");
+
+        try {
+            spinner.start();
+            const authData = await authPromise;
+            spinner.stop();
+            writeToConfigFile(authData);
+            logger.log(
+                `Authentication successful: wrote key to config file. To view it, type 'cat ~/${FILENAME}'.\n`,
+            );
+            server.close();
+            process.exit(0);
+        } catch (error) {
+            if (error instanceof UserCancellationError) {
+                server.close();
+                handleError("Authentication cancelled.\n");
+            } else {
+                server.close();
+                handleError(`Authentication failed: + ${error}`);
+            }
+        } finally {
+            server.close();
+            process.exit(0);
+        }
+    });
+
+
