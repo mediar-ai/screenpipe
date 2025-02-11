18a3196fc7a7260a6f40e817e623e9e7cb8fd576
matthew-heartful <matthew.heartful@gmail.com>
2025-02-11 15:58:51 -0800

    new root provider

--- pipes/meeting/src/app/meetings/page.tsx
+++ pipes/meeting/src/app/meetings/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 
 import { MeetingHistory } from "@/components/meeting-history/meeting-history"
-import { MeetingProvider } from "@/components/live-transcription/hooks/storage-for-live-meeting"
 
 export default function MeetingsPage() {
-  return (
-    <MeetingProvider>
-      <MeetingHistory />
-    </MeetingProvider>
-  )
+  return <MeetingHistory />
 }
