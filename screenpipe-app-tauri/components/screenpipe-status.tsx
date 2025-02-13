a4982c2feb90ea40fd1bd6e7a202db67e973d145
oliverqx <8559757+oliverqx@users.noreply.github.com>
2025-02-13 10:29:53 -0600

    feat(screenpipe-status): merge badges into one

--- screenpipe-app-tauri/components/screenpipe-status.tsx
+++ screenpipe-app-tauri/components/screenpipe-status.tsx
@@ -4,73 +4,39 @@ import { Badge } from "./ui/badge";
 import { Power, TriangleAlert } from "lucide-react";
 import { useStatusDialog } from "@/lib/hooks/use-status-dialog";
 import { useScreenpipeStatus } from "./screenpipe-status/context";
+import { SystemStatus } from "@/lib/hooks/use-health-check";
 
 const HealthStatus = () => {
-  const { isLoading, isServerDown } = useScreenpipeStatus();
-
-  if (isLoading) {
-    return <LoadingBadge />
-  }
+  const { isLoading, health } = useScreenpipeStatus();
+  const { open } = useStatusDialog();
 
   return (
-    <>
-      { isServerDown ? (
-        <ServerDownBadge />
+    <Badge 
+      variant="default"
+      data-status={health?.status}
+      className={"cursor-pointer bg-transparent text-foreground hover:bg-accent hover:text-accent-foreground"}
+      onClick={open}
+    >
+      { health?.status === SystemStatus.WEBSOCKET_CLOSED ? (
+        <TriangleAlert className="mr-2 h-4 w-4" />
       ) : (
-        <ServerUpBadge />
+        <Power className="mr-2 h-4 w-4" />
       )}
-    </>
-  );
-};
-
-function ServerDownBadge() {
-  const { open } = useStatusDialog();
-
-  return (
-    <Badge 
-      variant="default" 
-      className="cursor-pointer bg-transparent text-foreground hover:bg-accent hover:text-accent-foreground"
-      onClick={open}
-    >
-      <TriangleAlert className="mr-2 h-4 w-4" />
       <span
-        className={`ml-1 w-2 h-2 rounded-full animate-pulse  inline-block bg-yellow-300`}
-      />
-    </Badge>
-  );
-}
-
-function ServerUpBadge() {
-  const { health } = useScreenpipeStatus();
-  const { open } = useStatusDialog();
-
-  return (
-    <Badge 
-      variant="default" 
-      className="cursor-pointer bg-transparent text-foreground hover:bg-accent hover:text-accent-foreground"
-      onClick={open}
-    >
-      <Power className="mr-2 h-4 w-4" />
-      <span
-        data-status={health?.status}
+        data-status={isLoading ? "loading" : health?.status}
         className={`ml-1 w-2 h-2 rounded-full inline-block 
-          data-[status=unhealthy]:animate-pulse 
-          data-[status=error]:animate-pulse 
+          animate-pulse 
           data-[status=healthy]:bg-green-500 
           data-[status=unhealthy]:bg-red-500 
-          data-[status=error]:bg-red-500`
+          data-[status=error]:bg-red-500
+          data-[status=websocket_closed]:bg-yellow-300
+          data-[status=loading]:bg-gray-500
+          `
         }
       />
     </Badge>
   );
-}
+};
 
-function LoadingBadge() {
-  return (
-    <Badge variant="default" className="cursor-pointer bg-transparent text-foreground hover:bg-accent hover:text-accent-foreground">
-      <span className="ml-1 w-2 h-2 rounded-full inline-block animate-pulse bg-gray-500" />
-    </Badge>
-  );
-}
 
 export default HealthStatus;
