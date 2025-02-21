3562aef0d2364ffab4ce8703eb063d9f6c71911b
Louis Beaumont <louis.beaumont@gmail.com>
2025-02-21 14:54:04 -0800

    fix: spinner not working in cli, chore: make local dev easier

--- screenpipe-js/cli/src/commands/app/create.ts
+++ screenpipe-js/cli/src/commands/app/create.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import { input } from "@inquirer/prompts"; // Import select from inquirer prompts
 import { Command } from "commander";
 import inquirer from "inquirer";
@@ -14,40 +14,33 @@ const TEMPLATE_REPOS = {
 export const createAppCommand = new Command()
   .name("create")
   .description("create a new desktop app project")
-  .option(
-    "-a, --name <name>",
-    "the name of your app (optional)",
-  )
-  .option(
-    "-t, --appType <type>",
-    "the type of desktop app (electron or tauri)",
-  )
+  .option("-a, --name <name>", "the name of your app (optional)")
+  .option("-t, --appType <type>", "the type of desktop app (electron or tauri)")
   .action(async (options) => {
     let { name, appType } = options;
 
     if (!appType) {
       try {
         let { appTypePrompt } = await inquirer.prompt({
-            name: "appTypePrompt",
-            type: "select",
-            message: "what type of desktop app would you like to create?",
-            choices: [
-            { name: "electron", value: "electron"},
-            { name: "tauri", value: "tauri"}
+          name: "appTypePrompt",
+          type: "select",
+          message: "> what type of desktop app would you like to create?",
+          choices: [
+            { name: "electron", value: "electron" },
+            { name: "tauri", value: "tauri" },
           ],
-          default: "tauri"
+          default: "tauri",
         });
-        appType = appTypePrompt
+        appType = appTypePrompt;
       } catch (error) {
         handleError(error);
       }
     }
 
-
     if (!name || name.length === 0) {
       try {
         name = await input({
-          message: "What is your project name?",
+          message: "> what is your project name?",
           default: "my-desktop-app",
           validate: (input) => {
             if (input.trim().length === 0) return "project name is required.";
@@ -62,16 +55,19 @@ export const createAppCommand = new Command()
     const loadingSpinner = spinner("creating your desktop app...");
 
     try {
-      loadingSpinner.start();
-
-      await simpleGit().clone(TEMPLATE_REPOS[appType as "electron" | "tauri"], name);
-      loadingSpinner.succeed("Project created successfully! 🎉");
+      await simpleGit().clone(
+        TEMPLATE_REPOS[appType as "electron" | "tauri"],
+        name
+      );
+      loadingSpinner.succeed(`> project created successfully! 🎉`);
 
       logger.info("\ncredits to the template authors:");
       if (appType === "electron") {
         logger.info("electron template by: Neo @ https://github.com/neo773");
       } else {
-        logger.info("tauri template by: Lorenzo @ https://github.com/LorenzoBloedow");
+        logger.info(
+          "tauri template by: Lorenzo @ https://github.com/LorenzoBloedow"
+        );
       }
 
       logger.info("\nto get started:");
@@ -79,9 +75,11 @@ export const createAppCommand = new Command()
       logger.info("npm install     # or bun install, pnpm install, yarn");
       logger.info("npm run dev     # or bun dev, pnpm dev, yarn dev");
 
-      logger.info("\nwhen you're ready, you can deploy your app following the documentation for the respective framework.\n");
+      logger.info(
+        "\nwhen you're ready, you can deploy your app following the documentation for the respective framework.\n"
+      );
     } catch (error) {
-      loadingSpinner.fail("failed to create project");
+      loadingSpinner.failed("failed to create project");
       handleError(error instanceof Error ? error.message : String(error));
     }
   });
