c1d11f8a6d701318ecdcc5fd58646bb7b667ab6f
Ezra Ellette <ezrasellette@gmail.com>
2025-02-24 14:19:11 -0600

    move pcm_to_mel to audio crate

--- screenpipe-audio/src/utils/audio/mod.rs
+++ screenpipe-audio/src/utils/audio/mod.rs
@@ -1,11 +1,13 @@
 mod convert;
 mod normalization;
 mod pcm_decode;
+mod pcm_to_mel;
 mod resample;
 mod spectral_subtraction;
 
 pub use convert::audio_to_mono;
 pub use normalization::normalize_v2;
 pub use pcm_decode::pcm_decode;
+pub use pcm_to_mel::pcm_to_mel;
 pub use resample::resample;
 pub use spectral_subtraction::{average_noise_spectrum, spectral_subtraction};
