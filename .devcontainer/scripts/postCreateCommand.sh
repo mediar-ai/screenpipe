5e6cb8753f8366770e1380a14c438d483d164a18
divanshu-go <divanshugrover2009@gmail.com>
2025-03-11 21:56:20 +0530

    add scripts and devcontainer.json
    Signed-off-by: divanshu-go <divanshugrover2009@gmail.com>

---
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -0,0 +1,13 @@
+echo "Welcome to Screenpipe!"
+
+echo "We have set up an alias 'test-linux' for 'linux_integration.sh' in your shell configurations."
+echo "You can use 'test-linux' to check if the Linux build of the app is working."
+
+# Define the alias
+ALIAS_CMD="alias test-linux=\"${workspaceFolder}/linux_integration.sh\""
+
+# Add alias to .bashrc if it exists
+grep -qxF "$ALIAS_CMD" ~/.bashrc || echo "$ALIAS_CMD" >> ~/.bashrc
+
+# Add alias to .zshrc if it exists
+grep -qxF "$ALIAS_CMD" ~/.zshrc || echo "$ALIAS_CMD" >> ~/.zshrc
