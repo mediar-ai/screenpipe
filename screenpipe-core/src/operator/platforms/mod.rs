501dbaf2c438fc66726cfda225bca5f52897e69e
Divanshu Grover <divanshugrover2009@gmail.com>
2025-03-12 22:47:57 +0530

    fix windows build (#1585)

--- screenpipe-core/src/operator/platforms/mod.rs
+++ screenpipe-core/src/operator/platforms/mod.rs
@@ -4,7 +4,9 @@ use crate::operator::{AutomationError, Selector, UIElement};
 pub trait AccessibilityEngine: Send + Sync {
     /// Get the root UI element
     fn get_root_element(&self) -> UIElement;
-
+    
+    #[cfg(target_os = "windows")]
+    fn get_element_by_id(&self, _id: &str) -> Result<UIElement, AutomationError>;
     /// Get the currently focused element
     fn get_focused_element(&self) -> Result<UIElement, AutomationError>;
 
